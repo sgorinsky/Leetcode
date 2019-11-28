57e0e8c3b5e3ddfff9e62e985cd9a5420d443607
sgorinsky <sgorinsky@gmail.com>
2019-11-27 22:44:33 -0600

    class solution for tribonacci

--- 1137_NthTribonacciNumber.py
+++ 1137_NthTribonacciNumber.py
@@ -7,3 +7,12 @@ def tribonacci(num: int) -> int:
                 memo[n] = trib(n-1) + trib(n-2) + trib(n-3)
                 return memo[n]
         return trib(num)
+
+class Solution:
+    memo = {0: 0, 1: 1, 2: 1}
+    def tribonacci(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+        else:
+            self.memo[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+            return self.memo[n]

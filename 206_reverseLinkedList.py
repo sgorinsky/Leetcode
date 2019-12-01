39a064905f449cd8dd0fef594b3b2dc46b672547
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-01 12:09:31 -0600

    recursive reverse linked list

---
+++ 206_reverseLinkedList.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        current = head
+        temp = head.next
+        before = self.reverseList(temp)
+        current.next = None
+        temp.next = current
+        return before

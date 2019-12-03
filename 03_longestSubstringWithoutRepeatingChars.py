8285bf224473f3e665420348f5f766c722009f85
sgorinsky <sgorinsky@gmail.com>
2019-12-03 14:05:09 -0600

    slightly more compact better longest substr w/o repeating chars

--- 03_longestSubstringWithoutRepeatingChars.py
+++ 03_longestSubstringWithoutRepeatingChars.py
@@ -1,14 +1,13 @@
-def lengthOfLongestSubstring(s: str) -> int:
-
-    start = high = count = 0
-    letters = {}
-
-    for index, i in enumerate(s):
-        if i in letters and letters[i] >= start:
-            high = max(high, count)
-            count = index - letters[i]
-            start = letters[i]+1
-        else:
-            count += 1
-        letters[i] = index
-    return max(high, count)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        start = high = count = 0
+        letters = {}
+        
+        for index, i in enumerate(s):
+            if i in letters and letters[i] >= start:
+                high, count, start = max(high, count), index - letters[i], letters[i]+1
+            else:
+                count += 1
+            letters[i] = index
+        return max(high, count)

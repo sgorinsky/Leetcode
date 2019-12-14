f09faea2d873262b9708e42fa643921ac8843b58
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-14 12:41:47 -0600

    iterative merge two sorted linked lists

---
+++ 21_MergeTwoSortedLists.py
@@ -0,0 +1,37 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 == None and l2 == None:
+            return None
+        elif l1 == None:
+            return l2
+        elif l2 == None:
+            return l1
+        
+            
+        if l1.val > l2.val:
+            head = ListNode(l2.val)
+            l2 = l2.next
+        else:
+            head = ListNode(l1.val)
+            l1 = l1.next
+        head.next = None
+        current = head
+        
+        while l1 != None or l2 != None:
+            if l1 == None and l2 == None:
+                current.next = None
+            elif l1 == None:
+                current.next = ListNode(l2.val)
+                l2 = l2.next
+            elif l2 == None:
+                current.next = ListNode(l1.val)
+                l1 = l1.next
+            elif l1.val > l2.val:
+                current.next = ListNode(l2.val)
+                l2 = l2.next
+            else:
+                current.next = ListNode(l1.val)
+                l1 = l1.next
+            current = current.next
+            
+        return head

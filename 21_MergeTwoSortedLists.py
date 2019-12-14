47f11056ff40847c6641737249c7656b35ffacdf
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-14 12:46:41 -0600

    elegant recursive soln merge two linked lists

--- 21_MergeTwoSortedLists.py
+++ 21_MergeTwoSortedLists.py
@@ -35,3 +35,15 @@ class Solution:
             current = current.next
             
         return head
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+			# keep l1 always point to the smallest node
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+			# so each step, connect the current smallest node to the already linked list
+            l1.next = self.mergeTwoLists(l1.next, l2)
+		# short-circuit evaluation, if l1 not None, return l1, else, return l2
+        return l1 or l2

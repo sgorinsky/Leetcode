a2827ca6255ed8a7dd08c6d4e890fc537d2d8a50
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-24 13:33:06 -0600

    more efficient O(n) solution

--- 53_MaximumSubarray.py
+++ 53_MaximumSubarray.py
@@ -1,3 +1,18 @@
+# solution has a dragging pointer that makes sure no entries before it are negative
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum, current_sum, prior_stack_sum = -2147483648, 0, 0
+        for i in range(len(nums)):
+            prior_stack_sum, current_sum = current_sum, current_sum + nums[i]
+            if current_sum > max_sum:
+                max_sum = current_sum
+            if prior_stack_sum < 0:
+                current_sum = nums[i]
+                if current_sum > max_sum:
+                    max_sum = current_sum
+        return max_sum
+
+# inefficient solution: here to show how to improve it
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum, max_array = -2147483648, []

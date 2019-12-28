a620cd8327f7929fa628288d97c39d3132a16ffd
sgorinsky <sgorinsky@gmail.com>
2019-12-27 22:25:01 -0500

    not like instead of !=

--- 620_NotBoringMovies.sql
+++ 620_NotBoringMovies.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select *
 from cinema
-where mod(id, 2) = 1 and description != 'boring'
+where mod(id, 2) = 1 and description not like '%boring%'
 order by rating DESC
 ;

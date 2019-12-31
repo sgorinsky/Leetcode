f9191d1016076ff720fa935c6ec22598d66988ff
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-31 17:03:39 -0600

    closer to completing trapping rain water

--- 42_TrappingRainWater.py
+++ 42_TrappingRainWater.py
@@ -1,9 +1,14 @@
 class Solution:
     def trap(self, heights: List[int]) -> int:
         stack, length, current, total = [0], len(heights), 0, 0
-        
+
         for height in heights:
-            if height >= stack[-1]:
-                j = 0
-                while height >= stack[-1-j]:
-                    current
+            if stack and height >= stack[-1]:
+                while stack and height >= stack[-1]:
+                    current += (height - stack[-1])
+                    del stack[-1]
+                stack.append(height)
+                total, current, count = total+current, 0
+            else:
+                stack.append(height)
+        return total

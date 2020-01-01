970f7608b46a371405f6dc3ddaf032714bc9abb9
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-31 20:26:15 -0600

    other approach for trapping ran water

--- 42_TrappingRainWater.py
+++ 42_TrappingRainWater.py
@@ -8,7 +8,24 @@ class Solution:
                     current += (height - stack[-1])
                     del stack[-1]
                 stack.append(height)
-                total, current, count = total+current, 0
+                total, current = total+current, 0
             else:
                 stack.append(height)
+        return total
+
+class Solution:
+    def trap(self, heights: List[int]) -> int:
+        stack, length, current, total, last_current = [0], len(heights), 0, 0, 0
+        i = 0
+        while i < length:
+            j = i + 1
+            while j < length and i < j:
+                if heights[j] >= heights[i]:
+                    total += current
+                    i = j
+                else:
+                    current = current + (heights[i] - heights[j])
+                    j += 1
+            current = 0
+            i = j if i == j else i + 1
         return total

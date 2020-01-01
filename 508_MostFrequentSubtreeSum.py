7128348c646bffba9f07a369d764fb84e82473d9
Sam Gorinsky <sgorinsky@gmail.com>
2020-01-01 14:34:56 -0600

    verbose, idiosyncratic way of checking most frequent subtree sum

--- 508_MostFrequentSubtreeSum.py
+++ 508_MostFrequentSubtreeSum.py
@@ -6,37 +6,41 @@
 #         self.right = None
 
 class Solution:
-    total = 0
     all_values = {}
     current_highest_count = [0, []]
+    def findFrequentTreeSum(self, tree: TreeNode) -> List[int]:
+        self.all_values = {}
+        self.current_highest_count = [0, []]
     
-    def findFrequentTreeSum(self, root: TreeNode) -> List[int]:
-        if not root:
-            return root
-        current_sum = self.sumTree(root)
-        self.all_values[current_sum] = 1 if current_sum not in self.all_values else self.all_values[current_sum]+1
-        if self.all_values[current_sum] > self.current_highest_count[0]:
-            self.current_highest_count = [self.all_values[current_sum], [current_sum]]
-        elif self.all_values[current_sum] == self.current_highest_count[0]:
-            self.current_highest_count = [self.all_values[current_sum], self.current_highest_count[1] + [current_sum]]
-        
-        if root.left:
-            self.findFrequentTreeSum(root.left)
-        if root.right:
-            self.findFrequentTreeSum(root.right)
-        return self.current_highest_count[1]
-        
-    
-    def sumTree(self, tree: TreeNode) -> int:
-        self.total = 0
         def helper(root):
             if not root:
                 return root
-            self.total += root.val
+            current_sum = self.sumTree(root)
+            self.all_values[current_sum] = 1 if current_sum not in self.all_values else self.all_values[current_sum]+1
+            if self.all_values[current_sum] > self.current_highest_count[0]:
+                self.current_highest_count = [self.all_values[current_sum], [current_sum]]
+            elif self.all_values[current_sum] == self.current_highest_count[0]:
+                self.current_highest_count = [self.all_values[current_sum], self.current_highest_count[1] + [current_sum]]
+
+            if root.left:
+                helper(root.left)
+            if root.right:
+                helper(root.right)
+            return self.current_highest_count[1].copy()
+        num_list = helper(tree)
+        return num_list
+        
+    
+    def sumTree(self, tree: TreeNode) -> int:
+        def helper(root):
+            if not root:
+                return root
+            total[0] += root.val
             if root.left:
                 helper(root.left)
             if root.right:
                 helper(root.right)
             return root
+        total = [0]
         helper(tree)
-        return self.total
+        return total[0]

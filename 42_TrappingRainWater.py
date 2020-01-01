649b74c9a17cf6edfb7debc264d41212620694ba
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-31 21:46:47 -0600

    two main possibilities with twopointer approach

--- 42_TrappingRainWater.py
+++ 42_TrappingRainWater.py
@@ -1,3 +1,6 @@
+''' 
+# Original approach: this may be more optimizable down the road
+
 class Solution:
     def trap(self, heights: List[int]) -> int:
         stack, length, current, total = [0], len(heights), 0, 0
@@ -12,20 +15,32 @@ class Solution:
             else:
                 stack.append(height)
         return total
-
+'''
 class Solution:
     def trap(self, heights: List[int]) -> int:
         stack, length, current, total, last_current = [0], len(heights), 0, 0, 0
-        i = 0
-        while i < length:
-            j = i + 1
-            while j < length and i < j:
-                if heights[j] >= heights[i]:
-                    total += current
-                    i = j
-                else:
-                    current = current + (heights[i] - heights[j])
-                    j += 1
-            current = 0
-            i = j if i == j else i + 1
+        i, k = 0, length-1
+        while i < k:
+            if heights[i] <= heights[k]:
+                j = i + 1
+                while i < j < length:
+                    if heights[j] >= heights[i]:
+                        total += current
+                        i = j
+                    else:
+                        current += (heights[i] - heights[j])
+                        j += 1
+                current = 0
+                i = i+1 if i != j else j
+            else:
+                j = k - 1
+                while 0 <= j < k:
+                    if heights[j] >= heights[k]:
+                        total += current
+                        k = j
+                    else:
+                        current += (heights[k] - heights[j])
+                        j -= 1
+                current = 0
+                k = k-1 if k != j else j
         return total

dd39eb19dc8d6f78e8d83616ba9db268e6d57adf
Sam Gorinsky <sgorinsky@gmail.com>
2020-01-01 16:04:36 -0600

    initial attempt for cleaner solution with a postorder traversal

--- 508_MostFrequentSubtreeSum.py
+++ 508_MostFrequentSubtreeSum.py
@@ -44,3 +44,35 @@ class Solution:
         total = [0]
         helper(tree)
         return total[0]
+
+# Initial attempt at an O(N) approach with a postorder traversal
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    highest_count = [0, []]
+    def findFrequentTreeSum(self, tree: TreeNode) -> List[int]:
+        self.highest_count = [0, []]
+        num_list = {}
+        def helper(root, total):
+            if not root:
+                return root
+            if root.left:
+                total = helper(root.left, total)
+            if root.right:
+                total = helper(root.right, total)
+            total += root.val
+            print(f'root: {root.val}')
+            print(f'total: {total}')
+            num_list[total] = 1 if total not in num_list else num_list[total] + 1
+            if num_list[total] > self.highest_count[0]:
+                self.highest_count = [num_list[total], [num_list[total]]]
+            elif num_list[total] == self.highest_count[0]:
+                self.highest_count = [num_list[total], self.highest_count[1] + [total]]
+            return total
+        helper(tree, 0)
+        return self.highest_count[1]

e8aecb4f359d4ecda6f32e387e4f55ab31442fbe
Sam Gorinsky <sgorinsky@gmail.com>
2020-01-02 22:59:24 -0600

    looping through string backwards, no built-in helpers

--- 58_LengthOfLastWord.py
+++ 58_LengthOfLastWord.py
@@ -11,3 +11,21 @@ class Solution:
             return len(split_string[-1-i])
         else:
             return len(split_string[-1])
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        if not s:
+            return 0
+        
+        i, s = 0, ' ' + s
+        
+        while s[-1-i] == ' ' and i < len(s)-1:
+            i += 1
+            
+        if i < len(s)-1:
+            j = 0
+            while s[-1-i-j] != ' ':
+                j += 1
+            return j
+        else:
+            return 0

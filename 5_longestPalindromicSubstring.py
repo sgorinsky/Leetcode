5fe3842d4e317cb9bc4dfb8b6e75b9adb536906d
Sam Gorinsky <sgorinsky@gmail.com>
2020-01-12 19:53:46 -0600

    O(N^2) longest palindromic substring

--- 5_longestPalindromicSubstring.py
+++ 5_longestPalindromicSubstring.py
@@ -14,3 +14,27 @@ def longestPalindrome(s: str) -> str:
     
 def isPalindrome(s: string):
     return s == s[::-1]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_count, longest_string, string_length = 0, '', len(s)
+        for i, char in enumerate(s):
+            # odd strings
+            begin, end, curr_count, curr_string = i-1, i+1, 1, s[i]
+            while begin >= 0 and end < string_length and s[begin] == s[end]:
+                curr_string = s[begin] + curr_string + s[end]
+                curr_count += 2
+                begin -= 1
+                end += 1
+            if curr_count > longest_count:
+                longest_count, longest_string = curr_count, curr_string
+            # even strings
+            begin, end, curr_count, curr_string = i, i+1, 0, ''
+            while begin >= 0 and end < string_length and s[begin] == s[end]:
+                curr_string = s[begin] + curr_string + s[end]
+                curr_count += 2
+                begin -= 1
+                end += 1
+            if curr_count > longest_count:
+                longest_count, longest_string = curr_count, curr_string
+        return longest_string

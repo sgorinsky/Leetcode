4d741252fc56bcd28d71083d52f793893e4098ae
Sam Gorinsky <sgorinsky@gmail.com>
2020-01-24 21:15:36 -0600

    updated one pass binary search

--- 74_Search2DMatrix.py
+++ 74_Search2DMatrix.py
@@ -27,3 +27,21 @@ class Solution:
             else:
                 left = half+1
         return False
+
+# more efficient one-pass binary search
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or len(matrix[0]) == 0:
+            return False
+        row_length = len(matrix[0])
+        left, right = 0, row_length*(len(matrix)-1) + row_length - 1
+        while left <= right:
+            half = (left + right) >> 1
+            row, col = half//row_length, half%row_length
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                right = half - 1
+            else:
+                left = half + 1
+        return False

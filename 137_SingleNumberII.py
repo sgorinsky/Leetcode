28ae1b11a5eaa5212365b375e79132880c816b9b
Sam Gorinsky <sgorinsky@gmail.com>
2020-01-29 21:02:53 -0600

    xor approach with extra memory

--- 137_SingleNumberII.py
+++ 137_SingleNumberII.py
@@ -10,3 +10,15 @@ class Solution:
                     candidates.remove(n)
         for n in candidates:
             return n
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        past_nums, start = {}, 0
+        for n in nums:
+            if n not in past_nums:
+                past_nums[n] = 1
+                start ^= n
+            elif past_nums[n] == 1:
+                start ^= n
+                past_nums[n] += 1
+        return start

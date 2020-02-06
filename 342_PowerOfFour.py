b2c495441173b04ad1ddb91d42ce3614229524b3
sgorinsky <sgorinsky@gmail.com>
2020-02-05 23:57:37 -0600

    one-liner bit manipulation power of four

--- 342_PowerOfFour.py
+++ 342_PowerOfFour.py
@@ -12,3 +12,9 @@ class Solution:
             # Then screen out those numbers which are exact power of two only by 0xAAAAAAAA mask
             # Finally, we have number of (2^n)^k, where k >= 2
             return num & (num-1) == 0 and num & 0xAAAA_AAAA == 0
+
+# one-liner: if n>1: checks if power of 2 and if even power of 2
+class Solution:
+    def isPowerOfFour(self, num):
+        return num & (num-1) == 0 and num & 0xAAAA_AAAA == 0 if num > 1 else num == 1
+        

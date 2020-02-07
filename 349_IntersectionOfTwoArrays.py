34fc6b564d7a0058039188223b2b5e6b92fc3e5e
sgorinsky <sgorinsky@gmail.com>
2020-02-06 22:22:16 -0600

    better if clause

--- 349_IntersectionOfTwoArrays.py
+++ 349_IntersectionOfTwoArrays.py
@@ -1,8 +1,11 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        bigger_list = len(nums1) > len(nums2)
-        big_set = set(nums1) if bigger_list else set(nums2)
-        smaller_list = nums2 if bigger_list else nums1
+        if len(nums1) > len(nums2):
+            big_set = set(nums1)
+            smaller_list = nums2
+        else:
+            big_set = set(nums2)
+            smaller_list = nums1
         intersection = set()
         for num in smaller_list:
             if num in big_set:

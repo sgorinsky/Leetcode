15f474c7aed087443753da6572d34921432b6152
sgorinsky <sgorinsky@gmail.com>
2020-02-06 22:24:56 -0600

    built-in set intersection

--- 349_IntersectionOfTwoArrays.py
+++ 349_IntersectionOfTwoArrays.py
@@ -11,3 +11,8 @@ class Solution:
             if num in big_set:
                 intersection.add(num)
         return intersection
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1, set2 = set(nums1), set(nums2)
+        return list(set2 & set1)

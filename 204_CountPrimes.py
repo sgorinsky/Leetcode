564fc858e1ef529b24c7a40acfccad0ccb449e69
sgorinsky <sgorinsky@gmail.com>
2020-02-09 20:58:29 -0600

    faster sieve with slicing worse space complexity

--- 204_CountPrimes.py
+++ 204_CountPrimes.py
@@ -9,3 +9,12 @@ class Solution:
                     j += 1
                 count += 1
         return count
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        sieve, count = [True] * n, 0
+        for i in range(2, n):
+            if sieve[i]:
+                sieve[i:n:i] = [False for idx in range(i, n, i)]
+                count += 1
+        return count

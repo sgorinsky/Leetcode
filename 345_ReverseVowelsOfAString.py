4a85c2a320f0c1dce9df72cdbe9079e9c32183bc
sgorinsky <sgorinsky@gmail.com>
2020-02-11 11:12:12 -0600

    much worse runtime (still O(N)), slightly better memory (still O(N))

--- 345_ReverseVowelsOfAString.py
+++ 345_ReverseVowelsOfAString.py
@@ -13,3 +13,20 @@ class Solution:
                 left += 1
                 right -= 1
         return ''.join(s)
+
+# much worse runtime (still O(N)), slightly better memory
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'}
+        left_str, right_str =  '', ''
+        while len(s) > 1:
+            if not s[0] in vowels:
+                left_str += s[0]
+                s = s[1:]
+            if not s[len(s)-1] in vowels:
+                right_str = s[len(s)-1] + right_str
+                s = s[:len(s)-1]
+            if len(s) > 1 and s[0] in vowels and s[len(s)-1]  in vowels:
+                left_str, right_str = left_str + s[len(s)-1], s[0] + right_str
+                s = s[1:len(s)-1]
+        return left_str + s + right_str

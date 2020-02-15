d558f89ae0cd4fe50506cc5082ea99277e1f02ce
sgorinsky <sgorinsky@gmail.com>
2020-02-15 11:41:00 -0600

    more compact

--- 520_DetectCapital.py
+++ 520_DetectCapital.py
@@ -1,3 +1,3 @@
 class Solution(object):
     def detectCapitalUse(self, word):
-        return True if word.isupper() or word.islower() or word.istitle() else False
+        return word.isupper() or word.islower() or word.istitle() 

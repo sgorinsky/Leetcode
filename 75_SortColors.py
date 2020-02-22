7e8c5c2ffa0d287c57d8fb1c42cf1a06edfe3070
Sam Gorinsky <sgorinsky@gmail.com>
2020-02-21 23:35:19 -0600

    slightly faster, iterated mutation instead of pythonic array slicing mutations

--- 75_SortColors.py
+++ 75_SortColors.py
@@ -14,3 +14,23 @@ class Solution:
                 
         nums[:reds], nums[reds:reds+whites], nums[reds+whites:reds+whites+blues] = [0] * reds, [1] * whites, [2] * blues
 	return nums # not needed since problem only asks for in-place mutation
+
+# iterated mutation instead of pythonic array mutation via array slicing
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        reds, whites, blues = 0, 0, 0
+        for n in nums:
+            if n == 0:
+                reds += 1
+            elif n == 1:
+                whites += 1
+            elif n == 2:
+                blues += 1
+                
+        for i in range(reds): nums[i] = 0
+        for i in range(reds, reds+whites): nums[i] = 1
+        for i in range(reds+whites, reds+whites+blues): nums[i] = 2
+

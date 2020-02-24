a87f087378a4e1c16fb9545c47667dad6817420b
Sam Gorinsky <sgorinsky@gmail.com>
2020-02-24 00:16:23 -0600

    slightly tighter solution

--- 59_SpiralMatrixII.py
+++ 59_SpiralMatrixII.py
@@ -20,3 +20,22 @@ class Solution:
                 
         return matrix
         
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[None]*n for i in range(n)]
+        row = col = count = 0
+        size = n-1
+        
+        for num in range(1, n**2+1):
+            if matrix[row][col]:
+                row = col = count = count+1
+                size -= 1
+            
+            matrix[row][col] = num
+            
+            if row == count and col < size: col += 1
+            elif row < size and col == size: row += 1
+            elif row == size and col > count: col -= 1
+            elif row > count and col == count: row -= 1
+                
+        return matrix

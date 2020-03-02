35ad98fa1ef3fa66ce339ede1ec4fb2e93691b78
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-02 11:25:29 -0600

    one pass for split and add to stack

--- 71_SimplifyPath.py
+++ 71_SimplifyPath.py
@@ -6,3 +6,24 @@ class Solution:
             if stack and list(d) == ['.','.']: stack.pop()
             elif d and list(d) != ['.'] and list(d) != ['.','.']: stack.append(d)
         return '/' + '/'.join(stack)
+
+# one pass for split
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        i, stack = 0, []
+        
+        while i < len(path):
+            word = ''
+            if path[i] == '/': i += 1
+            while i < len(path) and path[i] != '/':
+                word += path[i]
+                i += 1
+            if word == '..':
+                if stack: stack.pop()
+            elif word == '.':
+                continue
+            elif word:
+                stack.append(word)
+            i += 1
+        
+        return '/' + '/'.join(stack)

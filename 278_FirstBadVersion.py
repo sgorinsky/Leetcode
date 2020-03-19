d69d98308d48927e72fd9c5990b26500c81830c6
sgorinsky <sgorinsky@gmail.com>
2020-03-19 13:03:33 -0500

    another binary search soln first bad version

--- 278_FirstBadVersion.py
+++ 278_FirstBadVersion.py
@@ -21,3 +21,26 @@ class Solution:
                 else:
                     right = mid-1
         return right
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = (left + right)>>1
+            
+            if not isBadVersion(mid):
+                left = mid+1
+            elif not isBadVersion(mid-1):
+                return mid
+            else:
+                right = mid-1
+        return right

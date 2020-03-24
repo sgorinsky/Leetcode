af79b1c2ab09116758b7bfa08d0e3d16e85d7f99
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-23 23:30:21 -0500

    O(N) time and space

---
+++ 389_FindTheDifference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        letters_dict = {}
+        for l in s:
+            letters_dict[l] = 1 if l not in letters_dict else letters_dict[l] + 1
+        
+        for l in t:
+            if l not in letters_dict or letters_dict[l] == 0:
+                return l
+            letters_dict[l] -= 1
+        return ''

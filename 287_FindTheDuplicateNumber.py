8e43c2ccbddeec0c9be2d4d4069964a3d096e615
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-24 20:28:16 -0500

    larger than O(1) space, need to trim down solution

--- 287_FindTheDuplicateNumber.py
+++ 287_FindTheDuplicateNumber.py
@@ -4,3 +4,13 @@ class Solution:
         for i in range(1, len(nums)):
             if nums[i] == nums[i-1]: return nums[i]
         return -1
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums_set = set()
+        for n in nums:
+            if n in nums_set:
+                return n
+            else:
+                nums_set.add(n)
+        return -1

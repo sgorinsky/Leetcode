6e5eaf78fbbff0ec5d374b8ef00e4907d0fb79be
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-27 23:53:57 -0500

    fizz buzz

---
+++ 412_FizzBuzz.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        for i in range(1, n+1):
+            s = ''
+            if i % 3 == 0: s += 'Fizz'
+            if i % 5 == 0: s += 'Buzz'
+            
+            if not s: s = str(i)
+                
+            res.append(s)
+        return res

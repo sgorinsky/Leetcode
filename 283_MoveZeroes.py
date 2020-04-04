d9d2d969e05643ea6151b989c02510a3489ae442
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-04 11:17:09 -0500

    30 day challenge solution

--- 283_MoveZeroes.py
+++ 283_MoveZeroes.py
@@ -13,3 +13,20 @@ class Solution:
         for i in range(count):
             nums.append(0)
         return nums
+
+# 30 day challenge solution
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i, count = 0, 0
+        while i < len(nums):
+            if nums[i] == 0:
+                count += 1
+                del nums[i]
+            else:
+                i += 1
+        
+        for i in range(count):
+            nums.append(0)

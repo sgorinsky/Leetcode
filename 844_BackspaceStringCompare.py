cbcbc133ed3ecef215f83bbce945d05581c60142
sgorinsky <sgorinsky@gmail.com>
2020-04-09 17:59:07 -0500

    O(N) time, O(1) space for backspace string compare

--- 844_BackspaceStringCompare.py
+++ 844_BackspaceStringCompare.py
@@ -14,3 +14,22 @@ class Solution:
             else: t.append(T[i])
                 
         return s == t
+
+# O(1) space
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        i, j = len(S), len(T)
+        while i >= 0 and j >= 0:
+            i, j = self.findLastNotHash(S, i-1), self.findLastNotHash(T, j-1)
+            if (0 > i != j or 0 > j != i) or S[i] != T[j]: return False
+        return True
+    
+    def findLastNotHash(self, s, i):
+        hashes = 0
+        while i >= 0 and (s[i] == '#' or hashes):
+            if s[i] == '#':
+                hashes += 1
+            else:
+                hashes -= 1
+            i -= 1
+        return i

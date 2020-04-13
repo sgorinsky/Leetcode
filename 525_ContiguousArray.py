ab875490b6950d2b38effdd3a5b3f50dea5360a0
sgorinsky <sgorinsky@gmail.com>
2020-04-13 14:01:31 -0500

    store relative counts and when count already exists in hashmap, implies equal number of 0s and 1s in subarray

---
+++ 525_ContiguousArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        count = high = 0
+        counts_dict = {0: -1}
+        for i in range(len(nums)):
+            count = count-1 if nums[i] == 0 else count+1
+            
+            if count in counts_dict: 
+                high = max(high, i-counts_dict[count] )
+            else: 
+                counts_dict[count] = i
+
+        return high

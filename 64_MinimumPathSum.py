743dccae272353a5991186e52be7dbfb67181397
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-20 02:52:35 -0500

    dp soln min path sum O(mn) time and O(c) extra space

--- 64_MinimumPathSum.py
+++ 64_MinimumPathSum.py
@@ -17,22 +17,19 @@ class Solution:
         return helper(len(grid)-1, len(grid[0])-1)
 
 
-# iterative solution that doesn't compare top and left when row or col is 0
+# dynamic programming solution that mutates entries in array while iterating through grid
+# we know that the we just have to find the min of entries above and to the left when row > 0 and col > 0
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        if len(grid) <= 0 or grid is None:
-            return 0
-        rows = len(grid)
-        cols = len(grid[0])
-        
-        for i in range(rows):
-            for j in range(cols):
-                if r==c==0: # We just want to skip the top-left corner of the grid
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if row == col == 0:
                     continue
-                if r-1<0: # Cases for elements in top row
-                    grid[r][c] = grid[r][c] + grid[r][c-1]  
-                elif c-1<0: # Cases for elements in leftmost column
-                    grid[r][c] = grid[r][c] + grid[r-1][c]  
-                else: # Normal cell
-                    grid[r][c] = grid[r][c] + min(grid[r-1][c], grid[r][c-1])               
-        return grid[rows-1][cols-1] # We have got the minimum path accumaled at the bottom-right corner, just return this
+                elif row == 0:
+                    grid[row][col] = grid[row][col] + grid[row][col-1]
+                elif col == 0:
+                    grid[row][col] = grid[row][col] + grid[row-1][col]
+                else:
+                    grid[row][col] = grid[row][col] + min(grid[row-1][col], grid[row][col-1])
+            
+        return grid[len(grid)-1][len(grid[0])-1] if grid and grid[0] else 0

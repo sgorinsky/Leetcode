d10f1db960c1de61ac8b2474398585de7713ca3f
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-20 21:41:23 -0500

    O(2logN) find first and last pos of element in sorted array

--- 34_FindFirstAndLastPositionOfElementInSortedArray.py
+++ 34_FindFirstAndLastPositionOfElementInSortedArray.py
@@ -1,3 +1,25 @@
+# modified binary search to find first position of target and first position of target+1
+#	only care when nums[mid] >= target b/c we approach first found target from rt	
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first_pos = self.initialPos(nums, target)
+        last_pos = self.initialPos(nums, target+1)-1
+        
+        return [first_pos, last_pos] if first_pos <= last_pos else [-1, -1]
+
+        def initialPos(targ):
+            first_pos = len(nums)
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left + right) >> 1
+                if nums[mid] >= target:
+                    first_pos = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return first_pos
+                
+        
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> int:
         if not nums:

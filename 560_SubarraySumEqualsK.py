1ace03ada7002025102bd94ea0c44d32e1ae2b41
sgorinsky <sgorinsky@gmail.com>
2020-04-22 15:09:57 -0500

    O(N) time and space solution

--- 560_SubarraySumEqualsK.py
+++ 560_SubarraySumEqualsK.py
@@ -1,3 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = total = 0
+        prior_sums = {0: 1}
+        for i in range(len(nums)):
+            total += nums[i]
+            if total-k in prior_sums:
+                count += prior_sums[total-k]
+            prior_sums[total] = 1 if total not in prior_sums else prior_sums[total] + 1
+        return count
+
 # This O(N^2) time and O(1) space solution exceeds time limit but works
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:

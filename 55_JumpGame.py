cf9f045320f86f98c1d04556901865bd6bbf72d9
sgorinsky <sgorinsky@gmail.com>
2020-04-25 13:50:43 -0500

    forward pass JumpGame

--- 55_JumpGame.py
+++ 55_JumpGame.py
@@ -1,4 +1,16 @@
 # Leetcode Day 26
+# forward-pass, slightly messier than backwards traversal
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last = nums[0]
+        for i in range(len(nums)):
+            if nums[i] == 0 and last < i: # if can't get past 0, can't jump
+                return False
+            elif nums[i] + i > last and last >= i: # if we can get to nums[i]+i and it's greater than the current furthest point we can get to, that's the new furthest point
+                last = nums[i] + i
+
+        return last >= len(nums)-1
+
 # current_idx = i is cleaner
 class Solution:
     def canJump(self, nums: List[int]) -> bool:

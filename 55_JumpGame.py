2e8e512084b5510c10d5e48b8c4f7a71b9443acb
sgorinsky <sgorinsky@gmail.com>
2020-04-25 03:59:40 -0500

    almost same answer for Leetcode Day 26

--- 55_JumpGame.py
+++ 55_JumpGame.py
@@ -1,3 +1,13 @@
+# Leetcode Day 26
+# current_idx = i is cleaner
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        current_idx = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] >= current_idx-i:
+                current_idx = i
+        return current_idx == 0
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         length = len(nums)

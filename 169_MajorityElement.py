8cb8a3bbdbcb8c0c5666bb10cfc842153d4fec52
sgorinsky <sgorinsky@gmail.com>
2020-05-06 11:03:25 -0500

    Leetcode challenge may 6, more concise O(N) majority element

--- 169_MajorityElement.py
+++ 169_MajorityElement.py
@@ -1,5 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        counts = {}
+        for n in nums:
+            counts[n] = 1 if n not in counts else counts[n] + 1
+            if counts[n] > len(nums)//2:
+                return n
+        return 0
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
         num_dict, current_num, current_highest, half = {}, 0, 0, len(nums)//2
         
         for num in nums:

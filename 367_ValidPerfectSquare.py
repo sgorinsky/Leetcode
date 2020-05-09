60207e8284bba63ae3d087dd5fa9ade4793083a4
Sam Gorinsky <sgorinsky@gmail.com>
2020-05-09 10:53:51 -0500

    Leetcode may 9 O(logN) time is valid perfect square

--- 367_ValidPerfectSquare.py
+++ 367_ValidPerfectSquare.py
@@ -1,3 +1,19 @@
+# Leetcode May 9
+# more concise than recursive binary solution
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 0, num
+        while left <= right:
+            mid = (left+right)>>1
+            sq_mid = mid**2
+            if sq_mid == num:
+                return True
+            elif sq_mid > num:
+                right = mid-1
+            else:
+                left = mid+1
+        return False
+
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         return self.binarySearch(num, 0, round(num/2+0.5))

58958d8345f4cb1269a1de5693f1f4c9f6c7f416
sgorinsky <sgorinsky@gmail.com>
2020-06-10 21:13:11 -0500

    updating original - YOU USED A BRUTE FORCE SEARCH ON A SORTED ARRAY?!

--- 35_SearchInsertPosition.py
+++ 35_SearchInsertPosition.py
@@ -1,5 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left+right) >> 1
+            if mid < len(nums)-1 and nums[mid] < target < nums[mid+1]:
+                return mid+1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid -1
+        
+        return left + 1 if nums[left] < target else left
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         if len(nums) < 1:
             return 0
         i = 0

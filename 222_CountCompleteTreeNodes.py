19e29e5b7c1bc99eceb80f6e3ccc0a2562843fc2
Sam Gorinsky <sgorinsky@gmail.com>
2020-06-23 20:34:01 -0500

    more compact way of counting binary tree nodes

--- 222_CountCompleteTreeNodes.py
+++ 222_CountCompleteTreeNodes.py
@@ -6,6 +6,22 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
+        def traverse(node):
+            if not node:
+                return 0
+            
+            return 1 + traverse(node.left) + traverse(node.right)
+        
+        return traverse(root)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
         self.count = 0
         def traverse(node):
             if not node:

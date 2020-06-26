18f12011d944924b2cf67a2fee51d8e9d077abcc
Sam Gorinsky <sgorinsky@gmail.com>
2020-06-25 19:33:26 -0500

    self dp soln for min path sum

--- 64_MinimumPathSum.py
+++ 64_MinimumPathSum.py
@@ -1,3 +1,20 @@
+# DP solution
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i > 0 and j > 0:
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+                elif i > 0:
+                    grid[i][j] += grid[i-1][j]
+                elif j > 0:
+                    grid[i][j] += grid[i][j-1]
+        
+        return grid[-1][-1]
+
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:

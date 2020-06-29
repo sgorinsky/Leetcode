8ee7ac652455be6ced934b3d8f4c5e38aadfbb2d
sgorinsky <sgorinsky@gmail.com>
2020-06-29 14:16:57 -0500

    dp solution to unique paths

--- 62_UniquePaths.py
+++ 62_UniquePaths.py
@@ -1,4 +1,21 @@
 class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                if i > 0 and j > 0:
+                    dp[i][j] += dp[i-1][j] + dp[i][j-1]
+                elif i > 0:
+                    dp[i][j] = 1
+                elif j > 0:
+                    dp[i][j] = 1
+
+        return dp[-1][-1]
+
+class Solution:
     cache = {(0,0): 1, (1,1): 1}
     def uniquePaths(self, x: int, y: int) -> int:
         if x <= 0 or y <= 0:

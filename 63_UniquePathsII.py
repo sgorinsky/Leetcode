4345bf3aefd6d29abd5e3889bf7992b21d12e4cb
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-02 21:55:13 -0500

    dp solution for Unique Paths II

--- 63_UniquePathsII.py
+++ 63_UniquePathsII.py
@@ -1,3 +1,25 @@
+# DP solution -> obstacles indicate no acceptable path 
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if not (obstacleGrid and obstacleGrid[0]):
+            return 1
+        elif obstacleGrid[0][0] != 1:
+            obstacleGrid[0][0] = 1
+        else:
+            return 0
+        
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if obstacleGrid[i][j] == 1 and not i == j == 0:
+                    obstacleGrid[i][j] = 0
+                elif i > 0 and j > 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                elif i > 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j]
+                elif j > 0:
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1]
+        return obstacleGrid[-1][-1]
+
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         if not obstacleGrid:

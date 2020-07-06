44ba456df9c0af80898d24b363569f9af45e7533
sgorinsky <sgorinsky@gmail.com>
2020-07-06 11:27:31 -0500

    july 6, convert arr to int, add one, then convert back

--- 66_PlusOne.py
+++ 66_PlusOne.py
@@ -1,6 +1,23 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         if not digits:
+            return digits
+        
+        has_leading_zero = digits[0] == 0 and len(digits) > 1
+        to_int = 0
+        for i in range(len(digits)):
+            to_int = to_int*10 + digits[i]
+        
+        to_int += 1
+        res = []
+        while to_int != 0:
+            res = [to_int%10] + res
+            to_int //= 10
+        return res if not has_leading_zero else [0] + res
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if not digits:
             return [1]
         elif digits[-1] < 9:
             digits[-1] += 1

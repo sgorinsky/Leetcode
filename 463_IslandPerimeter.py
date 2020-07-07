6c16a1303f899043a3f01375c60f54bf9a100272
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-07 13:54:03 -0500

    july 7, iterate through matrix and find surrounding 0s, add to perimeter count

--- 463_IslandPerimeter.py
+++ 463_IslandPerimeter.py
@@ -1,5 +1,24 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    # row logic
+                    if i > 0 and grid[i-1][j] == 0 or i == 0:
+                        perimeter += 1
+                    if i < len(grid)-1 and grid[i+1][j] == 0 or i == len(grid)-1:
+                        perimeter += 1
+                    
+                    # col logic
+                    if j > 0 and grid[i][j-1] == 0 or j == 0:
+                        perimeter += 1
+                    if j < len(grid[0])-1 and grid[i][j+1] == 0 or j == len(grid[0])-1:
+                        perimeter += 1
+        return perimeter
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
         
         def getUnitPerim(row, col):
             count = 0

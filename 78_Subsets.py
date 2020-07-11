dad5065de85da6df715617138b0b2938b5bd731b
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-11 12:31:44 -0500

    recalling simpler Subsets soln where each n in nums is appended to all existing subsets in current resulting set for all n in nums

--- 78_Subsets.py
+++ 78_Subsets.py
@@ -20,10 +20,9 @@ class Solution:
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        output = [[]]
-        
-        for num in nums:
-            output += [curr + [num] for curr in output]
-        
-        return output
+	res = [[]]
+
+	for n in nums:
+	    res += [curr + [n] for curr in res]		
+
+	return res

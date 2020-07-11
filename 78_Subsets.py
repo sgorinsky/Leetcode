cfe8f8d14ded3f4caa886ce550df337f0b8a3b20
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-11 12:27:34 -0500

    backtracking soln for returning power set of a given set

--- 78_Subsets.py
+++ 78_Subsets.py
@@ -1,5 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        res, sets = [[]], set()
+        def helper(arr):
+            if not arr:
+                return res
+            
+            if tuple(sorted(arr)) not in sets:
+                sets.add(tuple(sorted(arr)))
+                res.append(sorted(arr))
+            else:
+                return res
+                
+            for i in range(len(arr)):
+                helper(arr[0:i] + arr[i+1:])
+            
+            return res
+        
+        return helper(nums)
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         output = [[]]
         

0cdb23ec5d519b76c833dba04d064454c8a45a19
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-15 12:38:48 -0500

    slightly tighter merge intervals

--- 56_MergeIntervals.py
+++ 56_MergeIntervals.py
@@ -6,14 +6,12 @@ class Solution:
         intervals.sort(key = lambda x: x[0])
 
         res, curr_interval, prev_end, append_end = [], [], None, False
-        
-        for i, [start, end] in enumerate(intervals):
-            if not curr_interval:
-                curr_interval = [start, end]
-            elif prev_end != None and prev_end >= start:
-                curr_interval = [min(curr_interval[0], start), max(curr_interval[1], end)]
+        for [start, end] in intervals:
+            if prev_end != None and prev_end >= start:
+                curr_interval = [curr_interval[0], max(curr_interval[1], end)]
             else:
-                res.append(curr_interval)
+                if curr_interval:
+                    res.append(curr_interval)
                 curr_interval = [start, end]
             
             prev_end = max(end, prev_end) if prev_end else end

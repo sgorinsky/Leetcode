85a4c8326483df36703d80802aa7b9ed69d5301f
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-18 09:09:02 -0500

    left.right -> root.right then immediately point root.right -> left

--- python/114_FlattenBinaryTreeToLinkedList.py
+++ python/114_FlattenBinaryTreeToLinkedList.py
@@ -11,6 +11,30 @@ class Solution:
         """
         if not root:
             return None
+        if not root.left and not root.right:
+            return root
+        
+        left, right = self.flatten(root.left), self.flatten(root.right)
+        
+        if left:
+            left.right = root.right
+            root.left, root.right = None, root.left
+        
+        return right if right else left
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return None
         
         left = self.flatten(root.left) if root.left else None
         right = self.flatten(root.right) if root.right else None

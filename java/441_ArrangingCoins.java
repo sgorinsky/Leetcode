f70a9c086e71320b313b0cca069068c7daba1132
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-19 01:21:10 -0500

    arrange coins in staircase

---
+++ java/441_ArrangingCoins.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        if (n < 0) {
+            return -1;
+        }
+        int i = 0;
+        int k = 1;
+        int count = 0;
+        
+        while (n > 0) {
+            if (k <= n) {
+                ++count;
+            }
+            n -= k;
+            ++k;
+        }
+        return count;
+    }
+}

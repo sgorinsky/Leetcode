408c0271509baa24a03de84a27eaaec612edf7cf
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-19 23:36:41 -0500

    iterate through graph[idx] by '(int node : graph[idx])' instead of '(int i = 0; i < graph[idx].length; ++i)'

--- java/797_AllPathsFromSourceToTarget.java
+++ java/797_AllPathsFromSourceToTarget.java
@@ -1,5 +1,5 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    private List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<Integer> start = new ArrayList<Integer>();
         start.add(0);
@@ -7,10 +7,9 @@ class Solution {
         return this.res;
     }
     public void dfs(int idx, List<Integer> currList, int[][] graph) {
-        int[] nodes = graph[idx];
-        for (int i = 0; i < nodes.length; ++i) {
-            currList.add(nodes[i]);
-            dfs(nodes[i], currList, graph);
+        for (int node : graph[idx]) {
+            currList.add(node);
+            dfs(node, currList, graph);
             currList.remove(currList.size()-1);
         }
         if (idx == graph.length-1) {

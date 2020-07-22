33e0bd57671a50c2b5c173c54ec3ff2e504c12c5
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-22 00:23:55 -0500

    java max cons ones

---
+++ java/485_MaxConsecutiveOnes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxOnes = 0, currOnes = 0;
+        for (int n : nums) {
+            if (n == 1) {
+                ++currOnes;
+                if (currOnes > maxOnes) maxOnes = currOnes;
+            } else {
+                currOnes = 0;
+            }
+        }
+        return maxOnes;
+    }
+}

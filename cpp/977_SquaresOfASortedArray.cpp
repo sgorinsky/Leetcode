ef248bd1af10f29d577fd10c9ac9f608e28b63a8
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-22 00:57:39 -0500

    cpp sort squares of sorted array

---
+++ cpp/977_SquaresOfASortedArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> neg_stack;
+        int idx = 0; 
+        int num_negs = 0;
+        
+        while (idx < A.size() && A[idx] < 0) {
+            neg_stack.push_back(A[idx]*A[idx]);
+            ++idx; 
+            ++num_negs;
+        }
+
+        vector<int> res;
+        for (int i = num_negs; i < A.size(); ++i) {
+            int square = A[i] * A[i];
+            while (!neg_stack.empty() && square > neg_stack.back()) {
+                res.push_back(neg_stack.back());
+                neg_stack.pop_back();
+            }
+            res.push_back(square);
+        }
+        while (!neg_stack.empty()) {
+            res.push_back(neg_stack.back());
+            neg_stack.pop_back();
+        }
+        return res;
+    }
+};

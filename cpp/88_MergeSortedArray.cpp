ca4f222c494233b298f345ccb733b2e9a0318a3f
sgorinsky <sgorinsky@gmail.com>
2020-07-23 02:21:16 -0500

    rename soln

---
+++ cpp/88_MergeSortedArray.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int idx = n+m-1;
+        m--, n--;
+        while (idx >= 0) {
+            if (n < 0 && m < 0) {
+                return;
+            } else if (n >= 0 && m >= 0) {
+                nums1[idx--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+            } else {
+                nums1[idx--] = m >= 0 ? nums1[m--] : nums2[n--];
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int idx = 0;
+        for (int i = 0; i < n+m; ++i) {
+            if (idx < n) {
+                if (nums1[i] > nums2[idx]) {
+                    for (int j = n+m-2; j >= i; --j) nums1[j+1] = nums1[j];
+                    nums1[i] = nums2[idx++];
+                } else if (i >= m+idx) {
+                    while (idx < n) nums1[i++] = nums2[idx++];
+                }
+            } else {
+                return;
+            }
+        }
+    }
+};

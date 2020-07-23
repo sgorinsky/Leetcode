3b25721a18aae0ef0243737b189c3b7aacd3e602
sgorinsky <sgorinsky@gmail.com>
2020-07-23 02:18:48 -0500

    three pointers for m-1, n-1, m+n-1 -> insert in nums1[m+n-1] greater of element at p1 and p2

--- java/88_MergeSortedArray.java
+++ java/88_MergeSortedArray.java
@@ -1,5 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx = n+m-1;
+        m--; n--;
+        while (idx >= 0) {
+            if (n < 0 && m < 0) {
+                return;
+            } else if (n >= 0 && m >= 0) {
+                nums1[idx--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+            } else {
+                nums1[idx--] = m >= 0 ? nums1[m--] : nums2[n--];
+            }
+        }
+    }
+}
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
         int other_idx = 0;
         for (int i = 0; i < m + n; ++i) {
             if (other_idx < n) {

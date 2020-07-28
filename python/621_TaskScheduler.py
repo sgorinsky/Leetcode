7b38dfad3781c77c0f0ee529c4f451df02d452a2
Sam Gorinsky <sgorinsky@gmail.com>
2020-07-28 17:26:45 -0500

    task scheduler

---
+++ python/621_TaskScheduler.py
@@ -0,0 +1,15 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        frequencies = [0 for _ in range(26)]
+        
+        for t in tasks: frequencies[ord(t)-ord('A')] += 1
+        
+        # max frequency
+        f_max = max(frequencies)
+        # count the most frequent tasks
+        n_max = frequencies.count(f_max)
+
+        return max(len(tasks), (f_max - 1) * (n + 1) + n_max)
+        
+        
+        

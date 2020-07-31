4c08810f01ab77c2002e3b66d2cdcc21ee2e6e9f
sgorinsky <sgorinsky@gmail.com>
2020-07-31 11:27:27 -0500

    memo fib approach

---
+++ cpp/70_ClimbingStairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    unordered_map<int, int> memo;
+    int climbStairs(int n) {
+        if (n <= 1) return 1;
+        else if (memo.find(n) != memo.end()) return memo[n];
+        
+        memo[n] = climbStairs(n-1) + climbStairs(n-2);
+        return memo[n];
+    }
+};

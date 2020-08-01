b7161085c2ee00a548e3b6daa698a9d722cef6d7
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-01 10:11:29 -0500

    without checker helper methods

--- python/520_DetectCapital.py
+++ python/520_DetectCapital.py
@@ -1,3 +1,18 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        isUpper = True
+        for i, l in enumerate(word):
+            if i == 0:
+                isUpper = l == l.upper()
+            elif isUpper:
+                if i == 1 and l == l.lower():
+                    isUpper = False
+                elif l != l.upper():
+                    return False
+            elif l != l.lower():
+                return False
+        return True
+
 class Solution(object):
     def detectCapitalUse(self, word):
         return word.isupper() or word.islower() or word.istitle() 

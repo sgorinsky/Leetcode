39bd9da1847676d17d38bfcb758e61e6edff73d2
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-04 19:11:32 -0500

    java power of 4

---
+++ java/342_PowerOfFour.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if (num == 1) return true;
+        int start = 1;
+        while (start <= num && start < 1073741824) {
+            start *= 4;
+            if (start == num) return true;
+        }
+        return false;
+    }
+}

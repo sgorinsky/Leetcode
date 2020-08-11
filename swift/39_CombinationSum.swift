7e93c4a73c0383e270ece503d70b2916863b5bd3
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-11 14:47:59 -0400

    recursive backtracking combination sum

---
+++ swift/39_CombinationSum.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var results = [[Int]]() 
+        var candidates = candidates.sorted() 
+        
+        func helper(_ curr: inout [Int], _ idx: Int, _ sum: Int) -> [[Int]] {
+            if sum == target {
+                results.append(curr)
+                return results
+            } else if sum > target {
+                return results
+            }
+            
+            for i in idx..<candidates.count {
+                let cand = candidates[i]
+                curr.append(cand)
+                helper(&curr, i, sum+cand)
+                curr.removeLast()
+            }
+            return results
+        }
+        
+        var curr = [Int]()
+        return helper(&curr, 0, 0)
+    }
+}

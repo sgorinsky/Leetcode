0faab47921fe5b0c9e0bcbfedd13eddad0d62623
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-11 11:16:38 -0400

    h-index: h entries >= h && N-h entries >= i

---
+++ cpp/274_HIndex.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), [](const int a, const int b) {
+            return a < b;
+        });
+        
+        int N = citations.size();
+        int h = 0;
+        for (int i = 0; i < N; ++i) {
+            int cand = citations[i];
+            if (cand >= i) {
+                if (N-cand >= i) {
+                    h = max(cand, h);
+                }
+            }
+            if (N-i <= cand) {
+                h = max(N-i, h);
+            }
+            
+        }
+        return h;
+    }
+};

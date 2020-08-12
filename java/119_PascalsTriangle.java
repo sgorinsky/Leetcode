ad3381233bd344a392726d16eaf2f869e08f5fb8
sgorinsky <sgorinsky@gmail.com>
2020-08-12 13:40:05 -0400

    constant mem k^2 dp build row approach

--- java/119_PascalsTriangle.java
+++ java/119_PascalsTriangle.java
@@ -1,4 +1,23 @@
 class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<Integer>(rowIndex+1){
+            {
+                add(1);
+            }
+        };
+        
+        
+        for (int i = 1; i <= rowIndex; ++i) {
+            for (int j = 1; j < i; ++j) {
+                row.set(j, j <= i/2 ? row.get(j) + row.get(i-j) : row.get(i-j));
+            }
+            row.add(1);
+        }
+        return row;
+    }
+}
+
+class Solution {
     public List<Integer> getRow(int n) {
         List<Integer> row = new ArrayList<>(){
             {

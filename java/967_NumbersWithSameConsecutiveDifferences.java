8593d0e1a38a411834cb5b0c0998b3e05a24b6ec
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-18 20:51:30 -0400

    slightly more efficient bfs with deque lls

--- java/967_NumbersWithSameConsecutiveDifferences.java
+++ java/967_NumbersWithSameConsecutiveDifferences.java
@@ -4,31 +4,30 @@ class Solution {
         if (N == 1)
             return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         
-        List<Integer> q = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Deque<Integer> q = new LinkedList<Integer>();
+        for (int i = 1; i <= 9; ++i)
+            q.add(i);
+        
         for (int i = 1; i < N; ++i) {
-            ArrayList<Integer> nextQ = new ArrayList<Integer>();
-            for (int n : q) {
-                int tail = n % 10;
-                ArrayList<Integer> digits = new ArrayList<Integer>();
+            Deque<Integer> nextQ = new LinkedList<Integer>();
+            while (!q.isEmpty()) {
+                int n = q.poll();
+                int d = n % 10;
                 
-                if (tail + K <= 9)
-                    digits.add(tail+K);
+                if (d - K >= 0)
+                    nextQ.add(n*10 + (d-K));
                 
-                if (K != 0 && tail-K >= 0)
-                    digits.add(tail-K);
-                
-                for (int d : digits)
-                    nextQ.add(n*10 + d);
+                if (K != 0 && d + K <= 9)
+                    nextQ.add(n*10 + (d+K));
             }
             q = nextQ;
         }
         
         int[] result = new int[q.size()];
         int idx = 0;
-        for (int n : q)
-            result[idx++] = n;
+        while (!q.isEmpty())
+            result[idx++] = q.poll();
         
         return result;
     }
-       
 }

fe8ab000763ceed5ce5ed210149730da44b75c74
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-26 22:58:34 -0400

    double pointer slow, fast check if cycle in linked list

--- python/141_LinkedListCycle.py
+++ python/141_LinkedListCycle.py
@@ -6,6 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
+        slow = fast = head
+        
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
         nodes = set()
         curr = head
         while curr:

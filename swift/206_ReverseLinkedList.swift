7ddc727d0e5bdf23b1824342dbf53e0460b55449
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-28 00:39:28 -0400

    iterative reversal too

--- swift/206_ReverseLinkedList.swift
+++ swift/206_ReverseLinkedList.swift
@@ -1,5 +1,19 @@
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
+        var curr: ListNode? = head, prev: ListNode? = nil
+        while curr != nil && curr?.next != nil {
+            var next = curr?.next
+            curr?.next = prev
+            prev = curr
+            curr = next
+        }
+        curr?.next = prev
+        return curr
+    }
+}
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
         if head == nil || head?.next == nil { return head }
         
         var newHead: ListNode? = reverseList(head?.next)

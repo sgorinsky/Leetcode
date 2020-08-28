32fcca8e948df21e2f006be3de3335ce886a7abf
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-27 23:08:44 -0400

    O(N) time, O(1) space intersection of two lls

---
+++ cpp/160_IntersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int a_size = 0, b_size = 0;
+        ListNode* curr_a = headA;
+        ListNode* curr_b = headB;
+        
+        while (curr_a || curr_b) {
+            if (curr_a) {
+                curr_a = curr_a->next;
+                ++a_size;
+            }
+            if (curr_b) {
+                curr_b = curr_b->next;
+                ++b_size;
+            }
+        }
+        
+        curr_a = headA, curr_b = headB;
+        if (b_size >= a_size) {
+            for (int i = 0; i < b_size-a_size; ++i)
+                curr_b = curr_b->next;
+        } else {
+            for (int i = 0; i < a_size-b_size; ++i)
+                curr_a = curr_a->next;
+        }
+        
+        while (curr_a && curr_b && curr_a != curr_b) {
+            curr_a = curr_a->next;
+            curr_b = curr_b->next;
+        }
+        return curr_a == curr_b ? curr_a : NULL;
+    }
+};

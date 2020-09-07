f04fd39bab49d44e54ab390d6f38dc5fbecfc9b4
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-06 22:50:40 -0400

    append pointer to primary buffer

--- cpp/157_ReadNCharactersGivenRead4.cpp
+++ cpp/157_ReadNCharactersGivenRead4.cpp
@@ -12,6 +12,30 @@ public:
      */
     int read(char *buf, int n) {
         int copiedChars = 0, readChars = 4;
+        
+        while (copiedChars < n && readChars == 4) {
+            // c++ allows pointer to append to primary buffer
+            readChars = read4(buf + copiedChars);
+            copiedChars += readChars;
+        }
+        return min(n, copiedChars);
+    }
+};
+
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int copiedChars = 0, readChars = 4;
         char buf4[4];
         
         while (copiedChars < n && readChars == 4) {

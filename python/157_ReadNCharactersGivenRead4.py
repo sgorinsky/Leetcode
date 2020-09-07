3c365c98a32f59e377120e982d867001fc2f9476
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-06 22:14:29 -0400

    buffer read given read4 api

---
+++ python/157_ReadNCharactersGivenRead4.py
@@ -0,0 +1,37 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        res, ans, N = 0, [], n
+        while N > 0:
+            curr = read4(buf)
+            N -= curr
+            res += curr
+            for c in buf[0:4]:
+                if c != ' ':
+                    ans.append(c)
+                else:
+                    break
+            if curr != 4:
+                break
+        final = res if res < n else res-(res-n)
+        buf[:final] = ans[:final]
+        return final

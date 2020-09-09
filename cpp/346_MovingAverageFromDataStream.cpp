30193d2887f69b090a1e6b20805ff0217aa6080e
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-08 23:18:14 -0400

    moving avg from data stream

--- cpp/346_MovingAverageFromDataStream.cpp
+++ cpp/346_MovingAverageFromDataStream.cpp
@@ -1,5 +1,33 @@
 class MovingAverage {
 public:
+    deque<int> nums;
+    int size;
+    double total;
+    /** Initialize your data structure here. */
+    MovingAverage(int sz) {
+        size = sz;
+        total = 0;
+    }
+    
+    double next(int val) {
+        if (nums.size() == size) {
+            total -= nums.front();
+            nums.pop_front();
+        }
+        total += val;
+        nums.push_back(val);
+        return total / nums.size();
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
+
+class MovingAverage {
+public:
     deque<int> queue = {};
     int size;
     double total = 0;

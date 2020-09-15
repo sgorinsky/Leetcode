c5c91a3e658af95db150bee7d47936a3d5195e21
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-14 22:08:46 -0400

    dp max trailing states

---
+++ java/198_HouseRobber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev = 0, curr = 0;
+        
+        for (int n : nums) {
+            int temp = curr;
+            curr = Math.max(prev + n, curr);
+            prev = temp;
+        }
+        return curr;
+    }
+}

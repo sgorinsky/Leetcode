b464480932ebdc16cf0f173629b01c97f0a988af
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-14 22:44:12 -0400

    dfs + memo

--- python/198_HouseRobber.py
+++ python/198_HouseRobber.py
@@ -1,5 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        cache = {}
+        def helper(idx, total):
+            if idx in cache:
+                return cache[idx]
+            elif idx < 0:
+                return total
+            elif idx == 0:
+                return total + nums[idx]
+            
+            cache[idx] = max(helper(idx-1, total), helper(idx-2, total) + nums[idx])
+            return cache[idx]
+        
+        return helper(len(nums)-1, 0)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         prev = curr = 0
         for n in nums: prev, curr = curr, max(prev + n, curr)
         return curr

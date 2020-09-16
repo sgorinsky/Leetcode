207d7136dd102f77b587961959a697209aa3b610
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-16 17:32:17 -0400

    implement prefix trie

--- python/208_ImplementTrie.py
+++ python/208_ImplementTrie.py
@@ -4,6 +4,58 @@ class Trie:
         """
         Initialize your data structure here.
         """
+        self.trie = {}
+        self.end = False
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self
+        for l in word:
+            if l not in node.trie:
+                node.trie[l] = Trie()
+            node = node.trie[l]
+        node.end = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self
+        for l in word:
+            if l in node.trie:
+                node = node.trie[l]
+            else:
+                return False
+        return node.end
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self
+        for p in prefix:
+            if p in node.trie:
+                node = node.trie[p]
+            else:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
         self.trie_set = set()
         self.trie_list = []
         

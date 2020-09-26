31eb7bc16d524c1d644ee0591790cb79b0d0df42
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-25 20:36:28 -0500

    functional swifty lexicographic sort

---
+++ swift/179_LargestNumber.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func largestNumber(_ nums: [Int]) -> String {
+        var numStrings: [String] = nums.map({ (num: Int) -> String in
+            return String(num)
+        })
+        numStrings.sort(by: { (a: String, b: String) -> Bool in
+            return a + b > b + a
+        })
+        
+        if numStrings[0] == "0" { return "0" }
+        return numStrings.reduce("", +)
+    }
+}

504072c5c02db8d7793c240f2f7c425755479a1d
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-26 18:14:11 -0500

    more compact extending end point of overlapping intervals

--- cpp/495_TeemoAttacking.cpp
+++ cpp/495_TeemoAttacking.cpp
@@ -3,6 +3,16 @@ public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         if (timeSeries.empty()) return 0;
         
+        int total = duration;
+        for (int i = 1; i < timeSeries.size(); ++i)
+            total += min(timeSeries[i] - timeSeries[i-1], duration);
+        return total;
+    }
+};
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         int total_time = 0, end = 0;
         for (int t : timeSeries) {
             total_time += min(t + duration - end, duration);

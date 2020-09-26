1b9690d4baba1e63d60ab2cf2eb604969058ce79
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-25 20:37:04 -0500

    more compact swift functional approach

--- swift/179_LargestNumber.swift
+++ swift/179_LargestNumber.swift
@@ -2,6 +2,18 @@ class Solution {
     func largestNumber(_ nums: [Int]) -> String {
         var numStrings: [String] = nums.map({ (num: Int) -> String in
             return String(num)
+        }).sorted(by: { (a: String, b: String) -> Bool in
+            return a + b > b + a
+        })
+        
+        return numStrings[0] == "0" ? "0" : numStrings.reduce("", +)
+    }
+}
+
+class Solution {
+    func largestNumber(_ nums: [Int]) -> String {
+        var numStrings: [String] = nums.map({ (num: Int) -> String in
+            return String(num)
         })
         numStrings.sort(by: { (a: String, b: String) -> Bool in
             return a + b > b + a

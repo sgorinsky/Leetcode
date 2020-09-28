700eec72275c069a3cc2241823f5e34c41d0d260
Sam Gorinsky <sgorinsky@gmail.com>
2020-09-27 20:45:27 -0500

    almost same solution from postorder and inorder construction, reverse preorder and inorder, build from left

---
+++ python/105_ConstructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def build(stop):
+            if preorder and inorder[-1] != stop:
+                root = TreeNode(preorder.pop())
+                root.left = build(root.val)
+                inorder.pop()
+                root.right = build(stop)
+                return root
+        
+        preorder.reverse()
+        inorder.reverse()
+        return build(None)

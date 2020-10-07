52378ccb167e68e5df8bccdfe1f199af55137c05
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-07 04:20:14 -0500

    two-pass O(1) space solution, find length, then rotate list

--- cpp/61_RotateList.cpp
+++ cpp/61_RotateList.cpp
@@ -1,3 +1,37 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || k == 0) return head;
+        
+        int length = 0;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        
+        // get length
+        while (curr) {
+            prev = curr;
+            curr = curr->next;
+            length++;
+        }
+        
+        int shift = k % length;
+        if (shift == 0) return head;
+        
+        prev->next = head; // end->next references head
+        curr = head;
+        
+        // traverse list to new end
+        while (length - 1 > shift) {
+            curr = curr->next;
+            length--;
+        }
+        
+        ListNode* new_head = curr->next; // set new head
+        curr->next = NULL; // set new end
+        return new_head;
+    }
+};
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {

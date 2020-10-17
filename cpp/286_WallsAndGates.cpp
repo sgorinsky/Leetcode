8533fd0041d7c8e116b01eae426f3bc3e01a9cc8
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-17 10:22:57 -0500

    optimization on bfs to reduce checking all possible connected empty-rooms b/c we bfs from each gate instead of only one at a time

--- cpp/286_WallsAndGates.cpp
+++ cpp/286_WallsAndGates.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     void wallsAndGates(vector<vector<int>>& rooms) {
+        queue<vector<int>> q;
+        for (int i = 0; i < rooms.size(); ++i) {
+            for (int j = 0; j < rooms[0].size(); ++j) {
+                if (rooms[i][j] == 0) q.push({i, j});
+            }
+        }
+        
+        while (!q.empty()) {
+            vector<int> curr = q.front();
+            q.pop();
+            
+            int i = curr[0], j = curr[1];
+            for (vector<int> direction : directions) {
+                int r = i + direction[0], c = j + direction[1];
+                if (can_go(r, c, i, j, rooms)) {
+                    rooms[r][c] = min(rooms[r][c], rooms[i][j] + 1);
+                    q.push({r, c});
+                }
+            }
+        }
+    }
+    
+    // r, c are possible next row, col, i, j are prev
+    bool can_go(int r, int c, int i, int j, vector<vector<int>>& rooms) {
+        return r >= 0 && r < rooms.size() && c >= 0 && c < rooms[0].size() && rooms[r][c] > 0 && rooms[r][c] > rooms[i][j];
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    void wallsAndGates(vector<vector<int>>& rooms) {
         for (int i = 0; i < rooms.size(); ++i) {
             for (int j = 0; j < rooms[0].size(); ++j) {
                 if (rooms[i][j] == 0) {

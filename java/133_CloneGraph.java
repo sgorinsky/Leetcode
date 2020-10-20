8660a19c57770a8161660ed06619b1d033195f86
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-20 13:04:09 -0500

    clone graph bfs q + visited hashmap

--- java/133_CloneGraph.java
+++ java/133_CloneGraph.java
@@ -1,3 +1,33 @@
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        
+        Node clone = new Node(node.val);
+        
+        HashMap<Node, Node> clonedNodesMap = new HashMap<Node, Node>();
+        clonedNodesMap.put(node, clone);
+        
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(node);
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            Node clonedCurr = clonedNodesMap.get(curr);
+            
+            for (Node nei : curr.neighbors) {
+                if (clonedNodesMap.get(nei) == null) {
+                    Node clonedNeighbor = new Node(nei.val);
+                    clonedNodesMap.put(nei, clonedNeighbor);
+                    clonedCurr.neighbors.add(clonedNeighbor);
+                    q.add(nei);
+                } else {
+                    clonedCurr.neighbors.add(clonedNodesMap.get(nei));
+                }
+            }
+        }
+        return clone;
+    }
+}
+
 /*
 // Definition for a Node.
 class Node {

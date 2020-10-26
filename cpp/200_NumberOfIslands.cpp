4427c70513c1d795872fd3a3d57cf360166325a8
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-25 23:53:49 -0500

    stack dfs number of islands

--- cpp/200_NumberOfIslands.cpp
+++ cpp/200_NumberOfIslands.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    clearIslands(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+    
+    void clearIslands(vector<vector<char>>& grid, int i, int j) {
+        vector<pair<int, int>> stack = {{i, j}};
+        while (!stack.empty()) {
+            pair<int, int> curr = stack.back();
+            stack.pop_back();
+            
+            i = curr.first;
+            j = curr.second;
+            grid[i][j] = '2';
+            
+            for (pair<int, int> direction : directions) {
+                int r = i + direction.first;
+                int c = j + direction.second;
+                if (canTraverseTo(grid, r, c)) {
+                    stack.push_back({r, c});
+                }
+            }
+        }
+    }
+    
+    bool canTraverseTo(vector<vector<char>>& grid, int i, int j) {
+        return i >= 0 && i < grid.size() &&
+            j >= 0 && j < grid[0].size() &&
+            grid[i][j] == '1';
+    }
+};
+
+class Solution {
+public:
     const vector<vector<int>> directions = {
         {0, 1},
         {1, 0},

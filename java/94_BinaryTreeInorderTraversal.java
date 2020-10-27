8e77933baa9de0c4c9e2770a259cda9d63964566
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-27 14:00:37 -0500

    iterative stack inorder traversal

---
+++ java/94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> results = new ArrayList();
+        
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            results.add(root.val);
+            root = root.right;
+        }
+        return results;
+    }
+}

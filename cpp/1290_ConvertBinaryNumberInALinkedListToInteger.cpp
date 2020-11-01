e9e7b236093a4589e7512920014f05ae1dfc75cd
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-01 10:36:00 -0600

    traverse ll, add head val, shift bits

---
+++ cpp/1290_ConvertBinaryNumberInALinkedListToInteger.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int n = 0;
+        ListNode* curr = head;
+        while (curr) {
+            n <<= 1;
+            n += curr->val;
+            curr = curr->next;
+        }
+        return n;
+    }
+};

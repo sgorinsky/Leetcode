64c88ce85c52b07deffdaa6a66d84fffd8dea058
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-05 01:10:03 -0600

    py level order traversal

---
+++ python/559_MaximumDepthOfN-AryTree.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        q = deque([root])
+        height = 0
+        while q:
+            sz = len(q)
+            for i in range(sz):
+                curr = q.popleft()
+                for child in curr.children:
+                    q.append(child)
+            height += 1
+        return height
+        

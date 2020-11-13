07fe8c8a3ba710f400e972b1442bc0a84b5ca50b
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-13 17:06:30 -0600

    populate next right pointer each node w/ level order traversal in bst

--- cpp/116_PopulatingNextRightPointersInEachNode.cpp
+++ cpp/116_PopulatingNextRightPointersInEachNode.cpp
@@ -1,3 +1,40 @@
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return root;
+        
+        deque<Node*> q = {root};
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; ++i) {
+                Node* curr = q.front();
+                q.pop_front();
+                if (curr->left) q.push_back(curr->left);
+                if (curr->right) q.push_back(curr->right);
+                
+                if (i < sz - 1) curr->next = q.front();
+            }
+        }
+        return root;
+    }
+};
+
 /*
 // Definition for a Node.
 class Node {

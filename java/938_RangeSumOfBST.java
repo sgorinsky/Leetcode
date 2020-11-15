985a26f4c290a51c95e841f18fb9c0f8efe6b3de
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-15 11:17:17 -0600

    two-line dfs tree sum

---
+++ java/938_RangeSumOfBST.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+        
+        return (root.val >= low && root.val <= high ? root.val : 0) +
+            rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}

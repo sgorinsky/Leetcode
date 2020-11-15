3cb5149a7cc2f697039396de7b089b46146bb34d
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-15 11:13:07 -0600

    dfs range sum bst

---
+++ cpp/938_RangeSumOfBST.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        return (root->val >= low && root->val <= high ? root->val : 0) + 
+            rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
+};

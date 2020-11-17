b5ead34650c17ac99f06c23d5754a2fd7a4f8551
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-17 00:57:44 -0600

    backtrack each NaN + short-circuit after checked all nums for given entry

---
+++ swift/37_SudokuSolver.swift
@@ -0,0 +1,47 @@
+class Solution {
+    func solveSudoku(_ board: inout [[Character]]) {
+        func checkCandidate(_ candidate: Character, _ row: Int, _ col: Int) -> Bool {
+            var rowInBox = row / 3 * 3
+            var colInBox = col / 3 * 3
+            for i in 0..<9 {
+                if i != col && board[row][i] == candidate {
+                    return false
+                }
+                if i != row && board[i][col] == candidate {
+                    return false
+                }
+                if rowInBox != row && colInBox != col && board[rowInBox][colInBox] == candidate {
+                    return false
+                }
+                rowInBox = row / 3 * 3 + i / 3
+                colInBox = col / 3 * 3 + i % 3
+            }
+            return true
+        }
+        
+        func backtrack(_ isComplete: Bool) -> Bool {
+            var checkIfComplete = isComplete
+            for i in 0..<9 {
+                for j in 0..<9 {
+                    if board[i][j] == "." {
+                        for n in 1...9 {
+                            var c = Character("\(n)")
+                            if checkCandidate(c, i, j) {
+                                board[i][j] = c
+                                checkIfComplete = backtrack(checkIfComplete)
+                                if !checkIfComplete {
+                                    board[i][j] = "."
+                                } else {
+                                    return true
+                                }
+                            }
+                        }
+                        return checkIfComplete
+                    }
+                }
+            }
+            return true
+        }
+        backtrack(false)
+    }
+}

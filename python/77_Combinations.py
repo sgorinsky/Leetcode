169d67a1ba9a64bb2746bf8c4011eb74a94c9667
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-18 22:34:30 -0600

    combinations

--- python/77_Combinations.py
+++ python/77_Combinations.py
@@ -1,5 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        def combine(start = 1, curr = []):
+            if len(curr) == k:
+                res.append(curr)
+            else:            
+                for i in range(start, n + 1):
+                    combine(i + 1, curr + [i])
+            return res
+        
+        res = []
+        return combine()
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
         def helper(current_list):
             if len(current_list) == k:

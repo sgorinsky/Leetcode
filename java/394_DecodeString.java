d663b85338c0e069eeb312c9d2505f3270788730
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-19 13:38:14 -0600

    tighter decoded string

--- java/394_DecodeString.java
+++ java/394_DecodeString.java
@@ -1,5 +1,40 @@
 class Solution {
     public String decodeString(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c != ']') {
+                stack.push(c);
+            } else {
+                List<Character> currentString = new ArrayList<Character>();
+                while (!stack.isEmpty() && stack.peek() != '[') {
+                    currentString.add(stack.pop());
+                }
+                
+                stack.pop(); // remove '['
+                int repeats = 0, power = 0;
+                while (!stack.isEmpty() && stack.peek() - '0' >= 0 && stack.peek() - '0' <= 9) {
+                    repeats = (stack.pop() - '0') * ((int) Math.pow(10, power++)) + repeats;
+                    
+                }
+                
+                for (int i = 0; i < repeats; ++i) {
+                    for (int j = currentString.size() - 1; j >= 0; --j) {
+                        stack.push(currentString.get(j));
+                    }
+                }
+            }
+        }
+        char[] result = new char[stack.size()];
+        for (int i = 0; i < stack.size(); ++i) {
+            result[i] = stack.get(i);
+        } 
+        
+        return new String(result);
+    }
+}
+
+class Solution {
+    public String decodeString(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ']') {

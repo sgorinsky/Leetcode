731f3fb8de2c41a2138db1312466ec4022c66473
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-19 03:44:18 -0600

    straightforward decode string with stack

--- cpp/394_DecodeString.cpp
+++ cpp/394_DecodeString.cpp
@@ -1,3 +1,41 @@
+class Solution {
+public:
+    unordered_set<string> digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    string decodeString(string s) {
+        vector<string> stack;
+        for (char c : s) {
+            if (c != ']') {
+                stack.push_back(string(1, c));
+            } else {
+                string curr = "";
+                while (!stack.empty() && stack.back() != "[") {
+                    curr += stack.back();
+                    stack.pop_back();
+                }
+                
+                stack.pop_back();
+                string num_str = "";
+                while (!stack.empty() && digits.find(stack.back()) != digits.end()) {
+                    num_str = stack.back() + num_str;
+                    stack.pop_back();
+                }
+                
+                int n = stoi(num_str);
+                for (int i = 0; i < n; ++i) {
+                    stack.push_back(curr);
+                }
+            }
+        }
+        
+        string result_str;
+        for (int i = 0; i < stack.size(); ++i) {
+            for (int j = stack[i].length() - 1; j >= 0; --j) {
+                result_str += stack[i][j];
+            }
+        }
+        return result_str;
+    }
+};
 
 class Solution {
 public:

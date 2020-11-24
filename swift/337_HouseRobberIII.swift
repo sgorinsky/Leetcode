f447253bf44d00c58cce23ed676fe85c96403f1e
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-23 23:57:10 -0600

    swifty binary tree house robber

---
+++ swift/337_HouseRobberIII.swift
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rob(_ root: TreeNode?) -> Int {
+        func helper(_ root: TreeNode?, _ robbed: Int, _ notRobbed: Int) -> [Int] {
+            if root == nil { 
+                return [0, 0]
+            }
+            
+            var left = helper(root?.left, robbed, notRobbed)
+            var right = helper(root?.right, robbed, notRobbed)
+            
+            var rob = left[1] + right[1] + root!.val
+            var notRob = max(left[0], left[1]) + max(right[0], right[1])
+            
+            return [rob, notRob]
+        }
+        
+        var result = helper(root, 0, 0)
+        return max(result[0], result[1])
+    }
+}

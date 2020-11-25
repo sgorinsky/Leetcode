719e88dc274f794fbdf2d2b9a1382ddbdec2bf42
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-25 05:18:41 -0600

    always keep mod k and check if n == 0

---
+++ java/1015_SmallestIntegerDivisibleByK.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestRepunitDivByK(int K) {
+        if (K % 2 == 0 || K % 5 == 0) return -1;
+        int n = 1;
+        int length = 1;
+        while (n < 1000000) {
+            if (n % K == 0 || n == 0) return length;
+            n = (n * 10 + 1) % K;
+            length++;
+        }
+        return -1;
+    }
+}

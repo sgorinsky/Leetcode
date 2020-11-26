727c80fd9db469f63cd94a99a66a5da50ca71e4f
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-25 23:29:00 -0600

    string indexing in swift is always a pain

---
+++ swift/17_LetterCombinationsOfAPhoneNumber.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func letterCombinations(_ digits: String) -> [String] {
+        if digits.isEmpty { return [String]() }
+        func helper(_ start: Int, _ currentString: String) {
+            if currentString.count == digits.count {
+                letterPermutations.append(currentString)
+                return
+            }
+            
+            var idx = digits.index(digits.startIndex, offsetBy: start)
+            for i in start..<digits.count {
+                var digit = digits[idx]
+                guard let letters = digitLetterMap[digit] else { return }
+                for letter in letters {
+                    helper(i + 1, currentString + String(letter))
+                }
+                idx = digits.index(after: idx)
+            }
+        }
+        
+        var letterPermutations = [String]()
+        var digitLetterMap: [Character : String] = ["2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"]
+        
+        helper(0, "")
+        return letterPermutations
+    }
+}

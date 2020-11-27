e01d26105996f4e7e037465835aef41b49c8f76d
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-26 19:35:18 -0600

    no offset idx each helper call, pass next string idx through

--- swift/17_LetterCombinationsOfAPhoneNumber.swift
+++ swift/17_LetterCombinationsOfAPhoneNumber.swift
@@ -1,18 +1,18 @@
 class Solution {
     func letterCombinations(_ digits: String) -> [String] {
         if digits.isEmpty { return [String]() }
-        func helper(_ start: Int, _ currentString: String) {
+        func helper(_ start: Int, _ currentString: String, _ currentIdx: String.Index) {
             if currentString.count == digits.count {
                 letterPermutations.append(currentString)
                 return
             }
             
-            var idx = digits.index(digits.startIndex, offsetBy: start)
+            var idx = currentIdx
             for i in start..<digits.count {
                 var digit = digits[idx]
                 guard let letters = digitLetterMap[digit] else { return }
                 for letter in letters {
-                    helper(i + 1, currentString + String(letter))
+                    helper(i + 1, currentString + String(letter), digits.index(after: idx))
                 }
                 idx = digits.index(after: idx)
             }
@@ -21,7 +21,7 @@ class Solution {
         var letterPermutations = [String]()
         var digitLetterMap: [Character : String] = ["2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"]
         
-        helper(0, "")
+        helper(0, "", digits.startIndex)
         return letterPermutations
     }
 }

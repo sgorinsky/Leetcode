1a1251a79453769f0570351ca675b54a3413b7a5
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-29 11:36:47 -0600

    more concise dfs

--- cpp/1306_JumpGameIII.cpp
+++ cpp/1306_JumpGameIII.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        unordered_set<int> seen_idxs;
-        return dfs(arr, seen_idxs, start);
+        return dfs(arr, start);
     }
-    
-    bool dfs(vector<int>& arr, unordered_set<int>& seen_idxs, int idx) {
-        if (idx < 0 || idx >= arr.size() || seen_idxs.find(idx) != seen_idxs.end()) {
+
+    bool dfs(vector<int>& arr, int idx) {
+        if (idx < 0 || idx >= arr.size() || arr[idx] < 0) {
             return false;
         } else if (arr[idx] == 0) {
             return true;
-        } 
-        
-        seen_idxs.insert(idx);
-        return dfs(arr, seen_idxs, idx - arr[idx]) || dfs(arr, seen_idxs, idx + arr[idx]);
+        }
+
+        arr[idx] = -arr[idx];
+        return dfs(arr, idx - arr[idx]) || dfs(arr, idx + arr[idx]);
     }
 };

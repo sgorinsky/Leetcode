9f87013fac5e4865ae23fd786e68cd9c635d9829
Sam Gorinsky <sgorinsky@gmail.com>
2020-11-30 23:55:13 -0600

    different initial approach for the skyline problem

--- cpp/218_TheSkylineProblem.cpp
+++ cpp/218_TheSkylineProblem.cpp
@@ -1,57 +1,40 @@
-class compare{
-public:
-    bool operator()(const vector<int> &lhs, const vector<int> &rhs) const {
-        if(lhs[2] == rhs[2]) return lhs[1] < rhs[1];
-        else return lhs[2] < rhs[2];
-    }
-};
-
 class Solution {
 public:
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        vector<vector<int>> skyline;
-        priority_queue<vector<int>, vector<vector<int>>, compare> pq;
-        int n = (int)buildings.size(), i = 0;
-        
-        while(i < n){
-            vector<int> b = buildings[i];
-            // check if the left vertex (b[0], b[2]) can be a candidate of the skyline. Yes if:  
-            // (1) skyline for coordinates < b[0] have been finalized. 
-            //     So you can now consider b as the "first" building in the subsequent skyline
-            // (2) (b[0], b[2]) is above the tallest building in queue, 
-            //     and b's left side is cutting through the tallest building in priority queue 
-            // however, watch for edge cases for consecutive rectangles with the same left side x
-            // we need to merge the overlapping left sides of them. 
-            // e.g. [1, 2, 1], [1, 2, 2], [1, 2, 3] => [1, 3], [2, 0]  
-            if(pq.empty() || b[0] <= pq.top()[1]){
-                if(pq.empty() || !skyline.size()) skyline.push_back({b[0], b[2]});
-                else if(pq.top()[2] < b[2]) {
-                    if(b[0] == skyline.back()[0]) skyline.back()[1]= b[2]; // edge case 
-                    else skyline.push_back({b[0], b[2]});
-                }
-                pq.push(b); // add right side to queue (for convenience, just add b), because right side contribution to skyline is depending on other rectangles (those in queue)
-                i++; // continue to next building
-            }
-
-            // check if right side of rectangles in queue contributes to the skyline
-            // if left side of building b is not cutting through the tallest building T(pq.top()) in queue
-            // then the right side of T should make the next turn in skyline by cutting through
-            // another rectangle in queue or touch the ground(if you consider ground as unlimited length and height 0, so T always has something to cut through)
-            while (!pq.empty() && (b[0] > pq.top()[1] || i == n)) {
-                int x = pq.top()[1];
-                // pop buildings in queue that building T doesn't cut through. 
-                // Those buildings are shorter than T, and their right sides are left to T's right side, 
-                // so their right sides don't contribute to the skyline.
-                while(!pq.empty() && pq.top()[1] <= x) pq.pop(); 
-                // the top of the queue now should have the tallest building that T cuts through
-                int y = pq.empty() ? 0 : pq.top()[2];
-                // so we find the next point in skyline
-                skyline.push_back({x, y});
-                // we continue this step until b starts cutting the tallest building in queue 
-                // (when queue becomes empty, we can consider b cuts through ground, so while loops stops too)
-            }
+        vector<vector<int>> critical_points;
+        for (vector<int>& b : buildings) {
+            critical_points.push_back({b[0], b[2], true});
+            critical_points.push_back({b[1], b[2], false});
         }
         
-        return skyline; 
+        sort(critical_points.begin(), critical_points.end(), [](const vector<int>& a, const vector<int>& b) {
+            if (a[0] != b[0]) return a[0] < b[0];
+            return (a[2] ? a[1] : -a[1]) > (b[2] ? b[1] : -b[1]);
+        });
+        
+        set<int> pq = {0};    
+        vector<vector<int>> res;
+        for (int i = 0; i < critical_points.size(); ++i) {
+            vector<int>& point = critical_points[i];
+            bool start = point[2];
+            int x = point[0], y = point[1];
+            int top_pq = (int) *pq.rbegin();
+            cout << "x: " << x << ", y: " << y << ", start? " << start << endl;
+            cout << "prior_top of pq: " << top_pq << endl;
+            if (start) {
+                // if (i == 0 || critical_points[i-1][1] != y) 
+                pq.insert(y);
+            } else {
+                if (i == critical_points.size() - 1 || !(!critical_points[i+1][2] && y == critical_points[i+1][1])) {
+                    pq.erase(y);
+                }
+            }
+            cout << "new_top of pq: " << *pq.rbegin() << endl;
+            if (top_pq != *pq.rbegin()) res.push_back({x, *pq.rbegin()});
+        }
+        return res;
     }
 };
+
+// [[2,9,10],[3,9,9],[5,12,12],[15,20,10],[19,24,8]]
+// [[1,2,5], [1,3,4],[4,6,5],[5,7,5],[6,8,5],[8,9,5]]

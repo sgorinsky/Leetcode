3cb1675834feae2ea67581b0c7bbaa401cf9f9ff
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-02 01:11:47 -0600

    sort crit points + set instead of priority q

--- cpp/218_TheSkylineProblem.cpp
+++ cpp/218_TheSkylineProblem.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        vector<vector<int>> critical_points;
-        for (vector<int>& b : buildings) {
-            critical_points.push_back({b[0], b[2], true});
-            critical_points.push_back({b[1], b[2], false});
+        vector<vector<int>> crit_points;
+        unordered_map<int, int> starts, ends;
+        for (const vector<int>& building : buildings) {
+            crit_points.push_back({building[0], building[2], true});
+            starts[building[0]] = building[2];
+            
+            crit_points.push_back({building[1], building[2], false});
+            ends[building[1]] = building[2];
         }
         
-        sort(critical_points.begin(), critical_points.end(), [](const vector<int>& a, const vector<int>& b) {
+        sort(crit_points.begin(), crit_points.end(), [](const vector<int>& a, const vector<int>& b) {
             if (a[0] != b[0]) return a[0] < b[0];
             return (a[2] ? a[1] : -a[1]) > (b[2] ? b[1] : -b[1]);
         });
         
-        set<int> pq = {0};    
-        vector<vector<int>> res;
-        for (int i = 0; i < critical_points.size(); ++i) {
-            vector<int>& point = critical_points[i];
+        vector<vector<int>> ans;
+        set<int> pq = {0};
+        unordered_map<int, int> height_counts; // covers corner cases
+        for (int i = 0; i < crit_points.size(); ++i) {
+            const vector<int>& point = crit_points[i];
+            int top_pq = *pq.rbegin();
+            int x = point[0], h = point[1];
             bool start = point[2];
-            int x = point[0], y = point[1];
-            int top_pq = (int) *pq.rbegin();
-            cout << "x: " << x << ", y: " << y << ", start? " << start << endl;
-            cout << "prior_top of pq: " << top_pq << endl;
             if (start) {
-                // if (i == 0 || critical_points[i-1][1] != y) 
-                pq.insert(y);
+                pq.insert(h);
+                if (height_counts.find(h) == height_counts.end()) height_counts[h] = 1;
+                else height_counts[h] += 1;
             } else {
-                if (i == critical_points.size() - 1 || !(!critical_points[i+1][2] && y == critical_points[i+1][1])) {
-                    pq.erase(y);
-                }
+                height_counts[h] -= 1;
+                if (height_counts[h] == 0 && (starts.find(x) == starts.end() || starts[x] != h)) pq.erase(h);
             }
-            cout << "new_top of pq: " << *pq.rbegin() << endl;
-            if (top_pq != *pq.rbegin()) res.push_back({x, *pq.rbegin()});
+            if (top_pq != *pq.rbegin()) ans.push_back({x, *pq.rbegin()});
         }
-        return res;
+        return ans;
     }
 };
-
-// [[2,9,10],[3,9,9],[5,12,12],[15,20,10],[19,24,8]]
-// [[1,2,5], [1,3,4],[4,6,5],[5,7,5],[6,8,5],[8,9,5]]

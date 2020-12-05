f749f8747c2fac4a06429abb528c7c11bcac51bf
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-05 09:51:55 -0600

    one-pass place flowers

---
+++ cpp/605_CanPlaceFlowers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int N = flowerbed.size();
+        for (int i = 0; i < N; ++i) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == N - 1 || flowerbed[i+1] == 0)) {
+                n--;
+                flowerbed[i] = 1;
+                if (n == 0) break;
+            }
+        }
+        return n <= 0;
+    }
+};

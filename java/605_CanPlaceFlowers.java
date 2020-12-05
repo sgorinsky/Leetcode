d426f6472b7c2e6ea09f8335894e991c8273cba7
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-05 10:03:49 -0600

    slight optimization skips unnecessary array check

--- java/605_CanPlaceFlowers.java
+++ java/605_CanPlaceFlowers.java
@@ -4,7 +4,7 @@ class Solution {
         for (int i = 0; i < N; ++i) {
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == N - 1 || flowerbed[i+1] == 0)) {
                 n--;
-                flowerbed[i] = 1;
+                flowerbed[i++] = 1;
                 if (n == 0) break;
             }
         }

a3e924abc78cfcba2f98493f72e47ecd67ddfe5e
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-04 22:44:08 -0600

    more succinct guess game

--- python/374_GuessNumberHigherOrLower.py
+++ python/374_GuessNumberHigherOrLower.py
@@ -1,19 +1,17 @@
 # The guess API is already defined for you.
+# @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
 # def guess(num: int) -> int:
 
 class Solution:
     def guessNumber(self, n: int) -> int:
         left, right = 1, n
-        mid = (left+right)>>1
-        
-        res = guess(mid)
-        
-        while res != 0:
-            mid = (left+right)>>1
-            res = guess(mid)
+        while left <= right:
+            mid = (left + right) >> 1
+            g = guess(mid)
             
-            if res == 0: return mid
-            elif res == -1: right = mid-1
-            else: left = mid+1
-        return mid
+            if g == 0: return mid
+            elif g == -1: right = mid - 1
+            else: left = mid + 1
+                
+        return left

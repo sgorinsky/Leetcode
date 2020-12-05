304201ad5eb03a442166d7d6d1dc6a1df38b33ae
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-05 09:53:04 -0600

    single scan can place flowers

---
+++ java/605_CanPlaceFlowers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int N = flowerbed.length;
+        for (int i = 0; i < N; ++i) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == N - 1 || flowerbed[i+1] == 0)) {
+                n--;
+                flowerbed[i] = 1;
+                if (n == 0) break;
+            }
+        }
+        return n <= 0;
+    }
+}

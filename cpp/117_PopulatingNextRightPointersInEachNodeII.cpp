0813097593c37a7ecd06e677bd4d0a0f5d411371
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-06 08:38:57 -0600

    tough O(1) space approach populating next rt ptrs in each node

--- cpp/117_PopulatingNextRightPointersInEachNodeII.cpp
+++ cpp/117_PopulatingNextRightPointersInEachNodeII.cpp
@@ -1,3 +1,39 @@
+// O(1) space
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return NULL;
+        Node* left = root;
+        while (left) {
+            Node* curr = left;
+            while (curr) {
+                // cout << curr->val << endl;
+                if (curr->left) {
+                    if (curr->right) {
+                        curr->left->next = curr->right;
+                        // cout << "here" << endl;
+                        // cout << curr->left->val << curr->left->next->val << endl;
+                    } else {
+                        Node* temp = curr->next;
+                        while (temp && !(temp->left || temp->right)) temp = temp->next;
+                        if (temp) curr->left->next = temp->left ? temp->left : temp->right;
+                    }
+                }
+                if (curr->right) {
+                    Node* temp = curr->next;
+                    while (temp && !(temp->left || temp->right)) temp = temp->next;
+                    if (temp) curr->right->next = temp->left ? temp->left : temp->right;
+                }
+                curr = curr->next;
+            }
+            // cout << "left: " << left->val << endl;
+            left = left->left ? left->left : left->right;
+            while (left && !left->left && !left->right) left = left->next;
+        }
+        return root;
+    }
+};
+
 class Solution {
 public:
     Node* connect(Node* root) {

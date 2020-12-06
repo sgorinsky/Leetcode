eb2f9497988b8f665582dd5190eeb330a7a80c0c
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-05 22:46:50 -0600

    Integer.toBinaryString() api then concat to result

---
+++ java/1680_ConcatenationOfConsecutiveBinaryNumbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    final int MOD = 1000000007;
+    public int concatenatedBinary(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            String binary = Integer.toBinaryString(i);
+            for (int j = 0; j < binary.length(); j++) {
+                result = (result * 2 + (binary.charAt(j) - '0')) % MOD;
+            }
+        }
+        return result;
+    }
+}

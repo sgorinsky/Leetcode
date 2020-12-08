d42e52a6735669da534136a853b381c0690ce61e
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-07 21:42:23 -0600

    recursive binary search to find peak element in arr

--- python/162_FindPeakElement.py
+++ python/162_FindPeakElement.py
@@ -1,5 +1,14 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        def search(left, right):
+            if left == right: return left
+            mid = left + (right - left) // 2
+            return search(left, mid) if nums[mid] > nums[mid + 1] else search(mid + 1, right)
+
+        return search(0, len(nums) - 1)
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
         end = len(nums)
         if end <= 1 : return 0
         for i in range(1, end-1):

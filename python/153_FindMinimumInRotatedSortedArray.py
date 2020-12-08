b60025bb97b066558ea967ba375fe2319e2c4602
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-07 22:19:31 -0600

    min in rotated sorted list

---
+++ python/153_FindMinimumInRotatedSortedArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) >> 1
+            if nums[mid] < nums[right]: right = mid
+            else: left = mid + 1
+        
+        return nums[left]

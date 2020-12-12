bcef5f14e9ca73294c5bb5f332e43c782e985db3
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-11 22:52:22 -0600

    improve on removal of triple dups

--- python/80_RemoveDuplicatesFromSortedArrayII.py
+++ python/80_RemoveDuplicatesFromSortedArrayII.py
@@ -1,5 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        j = count = 1
+        for i in range(1, len(nums)):
+            count = count + 1 if nums[i] == nums[i-1] else 1
+            if count < 3: 
+                nums[j] = nums[i]
+                j += 1
+        return j
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
         i = 0
         while i < len(nums):
             if i >= 2 and nums[i] == nums[i-1] == nums[i-2]:

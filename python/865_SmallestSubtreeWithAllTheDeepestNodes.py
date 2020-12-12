d7b74b67fc0bbfb9c07371fc083715ed6e637cbd
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-12 10:11:36 -0600

    py subtree w all deepest nodes

---
+++ python/865_SmallestSubtreeWithAllTheDeepestNodes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        def helper(node, depth):
+            nonlocal max_depth, lca
+            max_depth = max(depth, max_depth)
+            
+            if not node: return depth
+            
+            left, right = helper(node.left, depth + 1), helper(node.right, depth + 1)
+            if left == right and left == max_depth: lca = node
+            return max(left, right)
+        
+        max_depth, lca = 0, root
+        helper(root, 0)
+        return lca    

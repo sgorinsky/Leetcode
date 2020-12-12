00cfb9374fef11591d8c7279f628eaaedad22432
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-12 10:04:48 -0600

    swift lca of deepest leaves

---
+++ swift/1123_LowestCommonAncestorOfDeepestLeaves.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func lcaDeepestLeaves(_ root: TreeNode?) -> TreeNode? {
+        func helper(_ node: TreeNode?, _ depth: Int) -> Int {
+            greatestDepth = max(depth, greatestDepth)
+            if node == nil { return depth }
+            
+            var left = helper(node?.left, depth + 1)
+            var right = helper(node?.right, depth + 1)
+            
+            if left == right && left == greatestDepth { ancestor = node }
+            return max(left, right)
+        }
+        
+        var ancestor = root
+        var greatestDepth = 0
+        helper(root, 0)
+        return ancestor
+    }
+}

5d8e95ac320543e14defab9294651b0cfd23e3a8
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-14 20:59:59 -0600

    search range of target in sorted array

--- python/34_FindFirstAndLastPositionOfElementInSortedArray.py
+++ python/34_FindFirstAndLastPositionOfElementInSortedArray.py
@@ -1,3 +1,29 @@
+class Solution:
+    # returns leftmost (or rightmost) index at which `target` should be inserted in sorted
+    # array `nums` via binary search.
+    def extreme_insertion_index(self, nums, target, left):
+        lo = 0
+        hi = len(nums)
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] > target or (left and target == nums[mid]):
+                hi = mid
+            else:
+                lo = mid+1
+
+        return lo
+
+
+    def searchRange(self, nums, target):
+        left_idx = self.extreme_insertion_index(nums, target, True)
+
+        # assert that `left_idx` is within the array bounds and that `target`
+        # is actually in `nums`.
+        if left_idx == len(nums) or nums[left_idx] != target: return [-1, -1]
+        return [left_idx, self.extreme_insertion_index(nums, target, False)-1]
+
+
 # modified binary search to find first position of target and first position of target+1
 #	only care when nums[mid] >= target b/c we approach first found target from rt	
 class Solution:

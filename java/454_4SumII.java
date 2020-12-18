bf7aa1c689e278ea2c18445003a9fcd00d273e75
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-17 18:50:49 -0600

    slightly optimized generalized approach

--- java/454_4SumII.java
+++ java/454_4SumII.java
@@ -3,6 +3,34 @@ class Solution {
         int[][] arrs = {A, B, C, D};
         HashMap<Integer, Integer> sums = new HashMap<Integer, Integer>();
         HashMap<Integer, Integer> nextSums;
+        
+        for (int n : arrs[0])
+            sums.put(n, sums.getOrDefault(n, 0) + 1);
+
+        for (int i = 1; i < arrs.length - 1; ++i) {
+            nextSums = new HashMap<Integer, Integer>();
+            for (int k : sums.keySet()) {
+                int count = sums.get(k);
+                for (int n : arrs[i])
+                    nextSums.put(k + n, nextSums.getOrDefault(k + n, 0) + count);
+            }
+            sums = nextSums;
+        }
+        
+        int count = 0;
+        for (int n : arrs[arrs.length - 1])
+            if (sums.get(-n) != null) 
+                count += sums.get(-n);
+
+        return count;
+    }
+}
+
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int[][] arrs = {A, B, C, D};
+        HashMap<Integer, Integer> sums = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> nextSums;
         for (int n : arrs[0])
             sums.put(n, sums.getOrDefault(n, 0) + 1);
 

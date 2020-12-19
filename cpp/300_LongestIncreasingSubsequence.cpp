a9c088962471418023001aebf2e63b941f4b4d54
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-18 22:37:22 -0600

    monotonic increasing vector - generalization of linear scan for increasing triplet subsequence

---
+++ cpp/300_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> subseq;
+        for (int n : nums) {
+            if (subseq.empty() || n > subseq.back()) subseq.push_back(n);
+            else {
+                for (int i = 0; i < subseq.size(); ++i) {
+                    if ((n < subseq[i] && i == 0) || 
+                        (n < subseq[i] && n > subseq[i-1]))
+                        subseq[i] = n;
+                }
+            }
+        }
+        return subseq.size();
+    }
+};

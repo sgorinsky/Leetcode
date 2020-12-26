26df2f993aaa0740ad741efcff64f895b36a8fe3
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-26 15:06:18 -0600

    length of array is up to 10000

--- java/702_SearchInASortedArrayOfUnknownSize.java
+++ java/702_SearchInASortedArrayOfUnknownSize.java
@@ -8,6 +8,29 @@
 
 class Solution {
     public int search(ArrayReader reader, int target) {
+        int left = 0, right = 10000;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int n = reader.get(mid);
+            
+            if (n == target) return mid;
+            else if (n < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
+
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
         if (reader.get(0) == target) return 0;
         
         int left = 0, right = 1;

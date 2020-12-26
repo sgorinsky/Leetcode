407a7f79e829b9176b958e65c6105025c547ba2c
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-25 23:49:44 -0600

    binary search node in tree

--- swift/270_ClosestBinarySearchTreeValue.swift
+++ swift/270_ClosestBinarySearchTreeValue.swift
@@ -1,5 +1,22 @@
 class Solution {
     func closestValue(_ root: TreeNode?, _ target: Double) -> Int {
+        var node = root
+        var closestNode: TreeNode? = node
+        var closestDifference = Double(Int.max)
+        while node != nil {
+            var currentDifference = abs(Double(node!.val) - target)
+            if currentDifference < closestDifference {
+                closestDifference = currentDifference
+                closestNode = node
+            }
+            node = Double(node!.val) > target ? node?.left : node?.right
+        }
+        return closestNode!.val
+    }
+}
+
+class Solution {
+    func closestValue(_ root: TreeNode?, _ target: Double) -> Int {
         func traverse(_ node: TreeNode?) {
             guard let node = node else { return }
             

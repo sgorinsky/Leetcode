afdf89c92d2a96841fb56a01b103d5bd745cab05
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-26 15:20:54 -0600

    fast iterative

---
+++ cpp/50_Pow.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double ans = 1;
+        double curr = x;
+        for (long long i = N; i > 0; i /= 2) {
+            if (i % 2 == 1) {
+                ans *= curr;
+            }
+            curr *= curr;
+        }
+        return ans;
+    }
+};

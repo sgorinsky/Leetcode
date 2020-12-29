23298de400b29d2c40c075af3c206fbdaef2d838
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-28 23:39:43 -0600

    bfs too slow, only use as example for initial approach

--- cpp/754_ReachANumber.cpp
+++ cpp/754_ReachANumber.cpp
@@ -7,3 +7,23 @@ public:
         return target % 2 == 0 ? k : k + 1 + k % 2;
     }
 };
+
+// bfs too slow work towards above ^
+class Solution {
+public:
+    int reachNumber(int target) {
+        vector<int> positions = {0};
+        int step = 1;
+        while (!positions.empty()) {
+            vector<int> next;
+            for (int p : positions) {
+                if (p == target) return step - 1;
+                next.push_back(p - step);
+                next.push_back(p + step);
+            }
+            positions = next;
+            step++;
+        }
+        return -1;
+    }
+};

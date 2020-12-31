32312e4149302905a89082d74595f7223c9b195f
Sam Gorinsky <sgorinsky@gmail.com>
2020-12-30 23:55:25 -0600

    convert current state of "life" board to next state

---
+++ cpp/289_GameOfLife.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size();
+        vector<vector<int>> new_board(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int count = live_count(board, i, j);
+                convert_entry(new_board, board[i][j], i, j, count);
+            }
+        }
+        board = new_board;
+    }
+    
+    void convert_entry(vector<vector<int>>& board, int entry, int i, int j, int count) {
+        if (entry == 0) {
+            if (count == 3) board[i][j] = 1;
+        } else {
+            board[i][j] = count < 2 || count > 3 ? 0 : 1;
+        }
+    }
+    
+    int live_count(vector<vector<int>>& board, int i, int j) {
+        int count = 0;
+        // cardinals
+        if (i > 0) count += board[i - 1][j];
+        if (i < board.size() - 1) count += board[i + 1][j];
+        if (j > 0) count += board[i][j - 1];
+        if (j < board[0].size() - 1) count += board[i][j + 1];
+        
+        // diagonals
+        if (i > 0 && j > 0) count += board[i - 1][j - 1];
+        if (i > 0 && j < board[0].size() - 1) count += board[i - 1][j + 1];
+        if (i < board.size() - 1 && j > 0) count += board[i + 1][j - 1];
+        if (i < board.size() - 1 && j < board[0].size() - 1) count += board[i + 1][j + 1];
+        
+        return count;
+    }
+};

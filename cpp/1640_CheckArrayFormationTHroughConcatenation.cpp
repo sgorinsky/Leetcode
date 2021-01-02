c1d9d027f6db77fe7032de60d458c34d847083c8
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-01 19:57:58 -0600

    more concise check order of nested arr into flattened arr

--- cpp/1640_CheckArrayFormationTHroughConcatenation.cpp
+++ cpp/1640_CheckArrayFormationTHroughConcatenation.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        unordered_map<int, int> indices;
+        for (int i = 0; i < pieces.size(); ++i) {
+            for (int n : pieces[i]) {
+                indices[n] = i;
+            }
+        }
+        
+        int idx = 0;
+        while (idx < arr.size()) {
+            if (indices.find(arr[idx]) == indices.end()) return false;
+            for (int n : pieces[indices[arr[idx]]]) {
+                if (n != arr[idx++]) return false;
+            }
+        }
+        return indices.size() == arr.size();
+    }
+};
+
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
         if (pieces.empty()) return arr.empty();
         else if (arr.empty()) return pieces.empty();
         

9cf3c099c5c43cacf84024b881e6fae03f9a2fc9
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-01 20:49:17 -0600

    O(1) space game of life

--- cpp/289_GameOfLife.cpp
+++ cpp/289_GameOfLife.cpp
@@ -2,6 +2,54 @@ class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
         int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int count = live_count(board, i, j);
+                convert_entry(board, board[i][j], i, j, count);
+            }
+        }
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                } else if (board[i][j] == -1) {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    void convert_entry(vector<vector<int>>& board, int entry, int i, int j, int count) {
+        if (entry == 0) {
+            if (count == 3) board[i][j] = 2;
+        } else if (count < 2 || count > 3) {
+            board[i][j] = -1;
+        }
+    }
+    
+    int live_count(vector<vector<int>>& board, int i, int j) {
+        int count = 0;
+        // cardinals
+        if (i > 0) count += abs(board[i - 1][j]) % 2;
+        if (i < board.size() - 1) count += abs(board[i + 1][j]) % 2;
+        if (j > 0) count += abs(board[i][j - 1]) % 2;
+        if (j < board[0].size() - 1) count += abs(board[i][j + 1]) % 2;
+        
+        // diagonals
+        if (i > 0 && j > 0) count += abs(board[i - 1][j - 1]) % 2;
+        if (i > 0 && j < board[0].size() - 1) count += abs(board[i - 1][j + 1]) % 2;
+        if (i < board.size() - 1 && j > 0) count += abs(board[i + 1][j - 1]) % 2;
+        if (i < board.size() - 1 && j < board[0].size() - 1) count += abs(board[i + 1][j + 1]) % 2;
+        
+        return count;
+    }
+};
+
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size();
         vector<vector<int>> new_board(m, vector<int>(n, 0));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {

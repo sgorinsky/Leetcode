f6a0330717af172bd105768e1a4fd73f5cd1c459
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-03 07:02:21 -0600

    makeshift priority q, really just sorted q

--- python/1710_MaximumUnitsOnATruck.py
+++ python/1710_MaximumUnitsOnATruck.py
@@ -1,5 +1,15 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        q = deque(sorted(boxTypes, key=lambda x: -x[1]))
+        units = 0
+        while q and truckSize > 0:
+            box = q.popleft()
+            units += min(truckSize, box[0]) * box[1]
+            truckSize -= box[0]
+        return units
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         boxTypes.sort(key=lambda x: x[1])
         units = 0
         while boxTypes and truckSize > 0:

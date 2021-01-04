dd9666df3a98ff90ef66b6903637c21602ca9fea
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-03 19:31:11 -0600

    mem allocated bool arr for tracking used nums

---
+++ cpp/526_BeautifulArrangement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int count = 0;
+    int countArrangement(int n) {
+        const int N = n;
+        bool used[N + 1];
+        memset(used, false, N + 1);
+        return backtrack(n, 1, used);
+    }
+    
+    int backtrack(int n, int idx, bool used[]) {
+        if (idx > n) return ++count;
+        for (int i = 1; i <= n; ++i) {
+            if (!used[i] && (idx % i == 0 || i % idx == 0)) {
+                used[i] = true;
+                backtrack(n, idx + 1, used);
+                used[i] = false;
+            }
+        }
+        return count;
+    }
+};

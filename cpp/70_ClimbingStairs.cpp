58fda7b8022a158c038aacac3c8c23e16524c214
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-05 00:41:33 -0600

    dp int arr

--- cpp/70_ClimbingStairs.cpp
+++ cpp/70_ClimbingStairs.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    int climbStairs(int n) {
+        int stairs[n + 1];
+        for (int i = 0; i < n + 1; ++i) {
+            if (i > 1) stairs[i] = stairs[i - 1] + stairs[i - 2];
+            else stairs[i] = 1;
+        }
+        return stairs[n];
+    }
+};
+class Solution {
+public:
     unordered_map<int, int> memo;
     int climbStairs(int n) {
         if (n <= 1) return 1;

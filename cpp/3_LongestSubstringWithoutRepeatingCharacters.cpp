28f60eff33dc0b347a4d3b80cda651e41a429679
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-07 22:18:46 -0600

    int array storing letter counts

--- cpp/3_LongestSubstringWithoutRepeatingCharacters.cpp
+++ cpp/3_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int high = 0, idx = 0;
+        int letters[128] = {};
+        for (int i = 0; i < s.length(); ++i) {
+            if (++letters[s[i]] > 1) {
+                while (letters[s[i]] > 1)
+                    letters[s[idx++]]--;
+            }
+            high = max(high, i - idx + 1);
+        }
+        return high;
+    }
+};
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
         unordered_map<char, int> symbols;
         int start = 0, count = 0, high = 0;
         for (int i = 0; i < s.length(); ++i) {

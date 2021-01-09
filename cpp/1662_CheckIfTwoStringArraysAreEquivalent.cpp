85d0eec6cca0f12db3594aaf8696bcbecb012de7
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-08 22:53:34 -0600

    check array and string indices

--- cpp/1662_CheckIfTwoStringArraysAreEquivalent.cpp
+++ cpp/1662_CheckIfTwoStringArraysAreEquivalent.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int idx1 = 0, idx2 = 0;
+        int a = 0, b = 0;
+        while (idx1 < word1.size() && idx2 < word2.size()) {
+            while (a < word1[idx1].length() && b < word2[idx2].length()) {
+                if (word1[idx1][a++] != word2[idx2][b++]) return false;
+            }
+            if (a >= word1[idx1].length()) idx1++, a = 0;
+            if (b >= word2[idx2].length()) idx2++, b = 0;
+        }
+        return idx1 == word1.size() && idx2 == word2.size();
+    }
+};
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string first = "", second = "";
         for (int i = 0; i < word1.size(); ++i) {
             for (char c : word1[i]) {

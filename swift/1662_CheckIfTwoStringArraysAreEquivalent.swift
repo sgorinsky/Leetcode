1698a56b46a5ce01f0840a549ece05422626083c
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-08 23:54:31 -0600

    O(1) approach but string indexing in swift makes it ugly

--- swift/1662_CheckIfTwoStringArraysAreEquivalent.swift
+++ swift/1662_CheckIfTwoStringArraysAreEquivalent.swift
@@ -1,5 +1,34 @@
 class Solution {
     func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+        var first = 0, second = 0
+        var idxA = word1[first].startIndex, idxB = word2[second].startIndex
+        while first < word1.count && second < word2.count {
+            while idxA != word1[first].endIndex && idxB != word2[second].endIndex {
+                if word1[first][idxA] != word2[second][idxB] {
+                    return false
+                }
+                idxA = word1[first].index(after: idxA)
+                idxB = word2[second].index(after: idxB)
+            }
+            if idxA == word1[first].endIndex {
+                first += 1
+                if first < word1.count {
+                    idxA = word1[first].startIndex 
+                }
+            }
+            if idxB == word2[second].endIndex {
+                second += 1
+                if second < word2.count {
+                    idxB = word2[second].startIndex 
+                }
+            }
+        }
+        return first == word1.count && second == word2.count
+    }
+}
+
+class Solution {
+    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
         var a = "", b = ""
         for word in word1 {
             for letter in word {

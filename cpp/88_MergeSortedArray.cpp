1dba7ebed0a7268b2b6602303892e98548006498
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-11 13:36:34 -0600

    more succinct merge sorted arr

--- cpp/88_MergeSortedArray.cpp
+++ cpp/88_MergeSortedArray.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int idx = m + n - 1;
+        m--, n--;
+        
+        while (idx >= 0) {
+            if (m >= 0 && n >= 0) {
+                nums1[idx] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+            } else {
+                nums1[idx] = n >= 0 ? nums2[n--] : nums1[m--];
+            }
+            idx--;
+        }
+    }
+};
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int idx = n+m-1;
         m--, n--;
         while (idx >= 0) {

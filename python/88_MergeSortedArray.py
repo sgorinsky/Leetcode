1276adbcb8326f9ceb730f36cf0ee2a8cdbc1889
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-11 23:26:42 -0600

    merge sorted arrays from the back

---
+++ python/88_MergeSortedArray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        m -= 1
+        n -= 1
+        idx = len(nums1) - 1
+        while idx >= 0:
+            if m >= 0 and n >= 0:
+                if nums1[m] > nums2[n]:
+                    nums1[idx] = nums1[m]
+                    m -= 1
+                else:
+                    nums1[idx] = nums2[n]
+                    n -= 1
+            else:
+                if m >= 0:
+                    nums1[idx] = nums1[m]
+                    m -= 1
+                else:
+                    nums1[idx] = nums2[n]
+                    n -= 1
+            idx -= 1

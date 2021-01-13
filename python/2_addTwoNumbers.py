8eb7264f576f1730f6d7b6f1bf1c062395a60b69
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-13 00:19:12 -0600

    add lls

--- python/2_addTwoNumbers.py
+++ python/2_addTwoNumbers.py
@@ -1,3 +1,21 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = curr = prev = l1
+        remainder = 0
+        while l1 or l2:
+            total = remainder
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            remainder, curr.val = total // 10, total % 10
+            curr.next, prev = l1 if l1 else l2, curr
+            curr = curr.next
+        prev.next = ListNode(1) if remainder else None
+        return head
+
 class ListNode:
 	def __init__(self,x):
 		self.val = x

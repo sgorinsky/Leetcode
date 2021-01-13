14c688368f9b13a492b3b624c23978845b5a53a5
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-13 00:13:10 -0600

    add two lls iteratively, O(1) space

--- java/2_AddTwoNumbers.java
+++ java/2_AddTwoNumbers.java
@@ -1,3 +1,28 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = l1, curr = l1, prev = null;
+        int remainder = 0;
+        while (l1 != null || l2 != null) {
+            int sum = remainder, digit;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            remainder = sum / 10;
+            curr.val = sum % 10;
+            prev = curr;
+            curr.next = l1 != null ? l1 : l2;
+            curr = curr.next;
+        }
+        prev.next = remainder == 1 ? new ListNode(1) : null;
+        return head;
+    }
+}
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {

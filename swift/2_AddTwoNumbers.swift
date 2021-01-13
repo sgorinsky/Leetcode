bcf4cd2bb2f318ddd4f73a999560c564b5824f22
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-13 00:23:43 -0600

    iterative add lls

--- swift/2_AddTwoNumbers.swift
+++ swift/2_AddTwoNumbers.swift
@@ -1,5 +1,31 @@
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var firstList = l1, secondList = l2
+        var head = l1, curr = l1, prev = l1
+        var remainder = 0
+        while firstList != nil || secondList != nil {
+            var sum = remainder
+            if firstList != nil {
+                sum += firstList!.val
+                firstList = firstList?.next
+            }
+            if secondList != nil {
+                sum += secondList!.val
+                secondList = secondList?.next
+            }
+            curr!.val = sum % 10
+            remainder = sum / 10
+            curr?.next = firstList != nil ? firstList : secondList
+            prev = curr
+            curr = curr?.next
+        }
+        prev?.next = remainder == 1 ? ListNode(1) : nil
+        return head
+    }
+}
+
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         return addNodes(l1, l2, false)
     }
     

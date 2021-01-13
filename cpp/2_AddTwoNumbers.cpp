6eac54df484960589a4c65858fe0d33128953fb4
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-12 19:42:14 -0600

    iterative add 2 linked lists, no extra space

--- cpp/2_AddTwoNumbers.cpp
+++ cpp/2_AddTwoNumbers.cpp
@@ -1,3 +1,36 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = l1;
+        ListNode* curr = l1;
+        ListNode* prev = NULL;
+        int remainder = 0;
+        while (l1 || l2) {
+            int sum = remainder;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            int digit = sum % 10;
+            remainder = sum / 10;
+            
+            curr->val = digit;
+            curr->next = l1 ? l1 : l2;
+            prev = curr;
+            curr = curr->next;
+        }
+        if (remainder == 1) {
+            ListNode* end = new ListNode(remainder);
+            prev->next = end;
+        }
+        return head;
+    }
+};
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {

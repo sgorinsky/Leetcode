64368f1b4b1211633914704e266141a9afb9855e
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-15 11:29:04 -0600

    one-pass arr

--- cpp/1646_GetMaximumInGeneratedArray.cpp
+++ cpp/1646_GetMaximumInGeneratedArray.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int getMaximumGenerated(int n) {
+        if (n <= 1) 
+            return n;
+        
+        int dp[n + 1];
+        memset(dp, 0, n + 1);
+        dp[1] = 1;
+        
+        int high = 0;
+        for (int i = 2; i < n + 1; ++i) {
+            dp[i] = i % 2 == 0 ? dp[i / 2] : dp[i / 2] + dp[i / 2 + 1];
+            high = max(high, dp[i]);
+        }
+        return high;
+    }
+};
+
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
         if (n == 0) return 0;
         
         vector<int> dp(n + 1, 0);

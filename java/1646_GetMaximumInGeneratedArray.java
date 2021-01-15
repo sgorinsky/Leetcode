a33ebddb0d9450769d9d274b4940a0e80745500b
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-15 11:59:30 -0600

    slightly more concise get ith entry

--- java/1646_GetMaximumInGeneratedArray.java
+++ java/1646_GetMaximumInGeneratedArray.java
@@ -7,7 +7,7 @@ class Solution {
         
         int high = 0;
         for (int i = 0; i <= n; ++i) {
-            dp[i] = i% 2 == 0 ? dp[i / 2] : dp[i / 2] + dp[i / 2 + 1];
+            dp[i] = dp[i / 2] + (i % 2 == 1 ? dp[i / 2 + 1] : 0);
             high = Math.max(high, dp[i]);
         }
         return high;

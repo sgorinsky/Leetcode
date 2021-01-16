90341dcaf5760126562ecaabc92a60c5a3db51eb
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-16 10:45:59 -0600

    priority q of size k

--- cpp/215_KthLargestElementInArray.cpp
+++ cpp/215_KthLargestElementInArray.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        auto cmp = [](int a, int b) {
+            return a > b;
+        };
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+        for (int n : nums) {
+            pq.push(n);
+            while (pq.size() > k)
+                pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end(), [](int a, int b) {
             return a > b;
         });

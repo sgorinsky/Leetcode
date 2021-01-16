3482dbbbdc66cb04899239bc9e436b62086ec8a0
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-16 02:49:07 -0600

    binary search, right max is sqrt(INT_MAX)

--- cpp/367_ValidPerfectSquare.cpp
+++ cpp/367_ValidPerfectSquare.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    int MAX_RIGHT = 46340;
+    bool isPerfectSquare(int num) {
+        int left = 0, right = num < MAX_RIGHT ? num : MAX_RIGHT;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long cand = mid * mid;
+            if (cand == num)
+                return true;
+            else if (cand > num)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
     bool isPerfectSquare(int num) {
         int root = (int) sqrt(num);
         return root * root == num;

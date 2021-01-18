4cad44c958f7a97e418383b07e33db7dfc043485
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-18 07:49:33 -0600

    one-pass hashmap

--- cpp/1679_MaxNumberOfK-SumPairs.cpp
+++ cpp/1679_MaxNumberOfK-SumPairs.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        int res = 0;
+        for (int n : nums) {
+            if (counts.find(k - n) != counts.end() && counts[k - n] > 0) {
+                res++;
+                counts[k - n]--;
+            } else {
+                counts[n]++;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int left = 0, right = nums.size() - 1, count = 0;
         while (left < right) {

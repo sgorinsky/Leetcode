a38a945e4ab540bf916b386d10bce6c3079ba561
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-18 14:54:20 -0600

    sort + two-pointer

--- swift/1679_MaxNumberOfK-SumPairs.swift
+++ swift/1679_MaxNumberOfK-SumPairs.swift
@@ -1,5 +1,26 @@
 class Solution {
     func maxOperations(_ nums: [Int], _ k: Int) -> Int {
+        var sortedNums = nums.sorted()
+        var left = 0, right = sortedNums.count - 1
+        var count = 0
+        while left < right {
+            var total = sortedNums[left] + sortedNums[right]
+            if total == k {
+                left += 1
+                right -= 1
+                count += 1
+            } else if total > k {
+                right -= 1
+            } else {
+                left += 1
+            }
+        }
+        return count
+    }
+}
+
+class Solution {
+    func maxOperations(_ nums: [Int], _ k: Int) -> Int {
         var numCounts = [Int:Int]()
         var count = 0
         for n in nums {

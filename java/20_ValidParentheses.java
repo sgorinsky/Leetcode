b5318ded02185f32755e53b317686452dc1f56f2
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-20 21:47:49 -0600

    more succinct stack implementation

--- java/20_ValidParentheses.java
+++ java/20_ValidParentheses.java
@@ -1,4 +1,27 @@
 class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> closedParens = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Stack<Character> stack = new Stack<Character>();
+        for (Character c : s.toCharArray()) {
+            if (closedParens.get(c) == null) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || closedParens.get(c) != stack.peek()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+class Solution {
     HashMap<Character, Character> closedParens = new HashMap<Character, Character>() {{
         put(')', '(');
         put(']', '[');

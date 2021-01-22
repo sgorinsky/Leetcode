f5ea14f11398f4f30ab3f51580495d08310ca1bb
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-21 18:03:56 -0600

    handle strange bug when k < stack.size() (k - stack.size() < 0) that explodes k - stack.size() term in while loop condition

--- cpp/1673_FindTheMostCompetitiveSubsequence.cpp
+++ cpp/1673_FindTheMostCompetitiveSubsequence.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
+        vector<int> stack;
+        // cout << "k: " << k << endl;
+        for (int i = 0; i < nums.size(); ++i) {
+            // cout << (nums.size() - i) << " " << (stack.size() > k ? 0 : k - stack.size()) << " " << stack.size() << ",  ";
+            while (!stack.empty() && 
+                   stack.back() > nums[i] && 
+                   nums.size() - i > (stack.size() > k ? 0 : k - stack.size()))  // bug with negative int value processed from binary when k < stack.size()
+                stack.pop_back();
+            
+            stack.push_back(nums[i]);
+        }
+        cout << endl;
+        while (stack.size() > k)
+            stack.pop_back();
+        
+        return stack;
+    }
+    
+//     void print(vector<int>& arr) {
+//         for (int n : arr)
+//             cout << n << " ";
+        
+//         cout << endl;
+//     }
+};
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
         int N = nums.size();
         vector<int> stack;
         int i = 0;

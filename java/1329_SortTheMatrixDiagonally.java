43478e87d1b523d4db17da34b025f6e5b6c1df09
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-23 18:53:19 -0600

    simpler priority q instead of sorting

---
+++ java/1329_SortTheMatrixDiagonally.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        for (int i = 0; i < mat.length; ++i)
+            storeAndSortDiagonal(mat, i, 0);
+        
+        for (int j = 0; j < mat[0].length; ++j)
+            storeAndSortDiagonal(mat, 0, j);
+        
+        return mat;
+    }
+    
+    public void storeAndSortDiagonal(int[][] mat, int row, int col) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        
+        int i = row, j = col;
+        while (i < mat.length && j < mat[0].length) 
+            pq.add(mat[i++][j++]);
+        
+        while (!pq.isEmpty()) 
+            mat[row++][col++] = pq.poll();
+    }
+}

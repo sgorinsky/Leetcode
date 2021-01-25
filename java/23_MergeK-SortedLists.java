57e9693661fc07237adbe47741f20b23f019a5e5
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-24 21:15:17 -0600

    O(NlogK) divide and conquer

--- java/23_MergeK-SortedLists.java
+++ java/23_MergeK-SortedLists.java
@@ -1,3 +1,33 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) 
+            return null;
+        
+        return splitAndMerge(lists, 0, lists.length - 1);
+    }
+    
+    public ListNode splitAndMerge(ListNode[] lists, int start, int end) {
+        if (start >= end)
+            return lists[start];
+        
+        int mid = start + (end - start) / 2;
+        ListNode left = splitAndMerge(lists, start, mid);
+        ListNode right = splitAndMerge(lists, mid + 1, end);
+        
+        return merge(left, right);
+    }
+    
+    public ListNode merge(ListNode a, ListNode b) {
+        if (a == null || b == null)
+            return a == null ? b : a;
+        else if (a.val > b.val)
+            return merge(b, a);
+        
+        a.next = merge(a.next, b);
+        return a;
+    }
+}
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {

c3cb675c53d31b1fb026e7069ae423c3b302f7d0
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-25 22:14:49 -0600

    slightly faster order pq smallest to largest and pop when pq size exceeds k, return front of pq

--- java/1738_FindKthLargestXORCoordinateValue.java
+++ java/1738_FindKthLargestXORCoordinateValue.java
@@ -1,5 +1,24 @@
 class Solution {
     public int kthLargestValue(int[][] matrix, int k) {
+        PriorityQueue<Integer> xorsPQ = new PriorityQueue<Integer>((a, b) -> a - b);
+        int N = matrix.length, M = matrix[0].length;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                matrix[i][j] ^= (i > 0 ? matrix[i-1][j] : 0) ^ (j > 0 ? matrix[i][j-1] : 0);
+                xorsPQ.add(matrix[i][j]);
+                if (xorsPQ.size() > k)
+                    xorsPQ.poll();
+                if (i < N - 1 && j < M - 1)
+                    matrix[i+1][j+1] ^= matrix[i][j];
+            }
+        }
+
+        return xorsPQ.peek();
+    }
+}
+
+class Solution {
+    public int kthLargestValue(int[][] matrix, int k) {
         PriorityQueue<Integer> xorsPQ = new PriorityQueue<Integer>((a, b) -> b - a);
         int N = matrix.length, M = matrix[0].length;
         for (int i = 0; i < N; ++i) {

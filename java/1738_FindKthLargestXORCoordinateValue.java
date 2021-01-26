70f5fb6475e4c69e0c8958eb7ecca4724c3b001e
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-25 22:10:18 -0600

    priority q + dp

---
+++ java/1738_FindKthLargestXORCoordinateValue.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int kthLargestValue(int[][] matrix, int k) {
+        PriorityQueue<Integer> xorsPQ = new PriorityQueue<Integer>((a, b) -> b - a);
+        int N = matrix.length, M = matrix[0].length;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                matrix[i][j] ^= (i > 0 ? matrix[i-1][j] : 0) ^ (j > 0 ? matrix[i][j-1] : 0);
+                xorsPQ.add(matrix[i][j]);
+                if (i < N - 1 && j < M - 1)
+                    matrix[i+1][j+1] ^= matrix[i][j];
+            }
+        }
+        
+        while (--k > 0)
+            xorsPQ.poll();
+        
+        return xorsPQ.poll();
+    }
+}

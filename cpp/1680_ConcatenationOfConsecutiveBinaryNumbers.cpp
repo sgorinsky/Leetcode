ce9a642a6691b39832bff60f38e89df375006a0c
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-27 22:22:56 -0600

    finding correct data type for x

--- cpp/1680_ConcatenationOfConsecutiveBinaryNumbers.cpp
+++ cpp/1680_ConcatenationOfConsecutiveBinaryNumbers.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int mod = 1000000007;
+    int concatenatedBinary(int n) {
+        long x = 0;
+        int shift = 0, val = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (i == val) {
+                val *= 2;
+                shift++;
+            }
+            x = ((x << shift) | i) % mod;
+        }
+        return x;
+    }
+};
+
+class Solution {
+public:
     int concatenatedBinary(int n) {
         const int MOD = 1000000007;
         int length = 0;   // bit length of addends

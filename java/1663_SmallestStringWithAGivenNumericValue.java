fa0efb25bf7073d7c113613bac58abce5f90c8ef
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-28 22:03:52 -0600

    one-pass simple char arr

--- java/1663_SmallestStringWithAGivenNumericValue.java
+++ java/1663_SmallestStringWithAGivenNumericValue.java
@@ -1,6 +1,27 @@
 class Solution {
     public String getSmallestString(int n, int k) {
         char[] res = new char[n];
+        for (int i = n - 1; i >= 0; --i) {
+            int idx = i + 1;
+            if (idx == k) {
+                res[i] = 'a';
+                k--;
+            } else if (k - idx < 26) {
+                int l = 'a' + k - idx;
+                res[i] = (char) l;
+                k = i;
+            } else {
+                res[i] = 'z';
+                k -= 26;
+            }
+        }
+        return new String(res);
+    }
+}
+
+class Solution {
+    public String getSmallestString(int n, int k) {
+        char[] res = new char[n];
         for (int i = 0; i < n; ++i) res[i] = 'a';
         
         int score = n;

a3551ba0fb7b6ba9a7ef5c01752fa82d43e45106
Sam Gorinsky <sgorinsky@gmail.com>
2021-01-28 21:59:42 -0600

    concise char[] approach

--- cpp/1663_SmallestStringWithAGivenNumericValue.cpp
+++ cpp/1663_SmallestStringWithAGivenNumericValue.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
+        char *res = new char[n];
+        for (int i = n - 1; i >= 0; --i) {
+            int idx = i + 1;
+            if (idx == k) {
+                res[i] = 'a';
+                k--;
+            } else if (k - idx < 26) {
+                res[i] = 'a' + k - idx;
+                k = i;
+            } else {
+                res[i] = 'z';
+                k -= 26;
+            };
+        }
+        return string(res, n);
+    }
+};
+
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
         string start = "";
         int count = 0;
         for (int i = 0; i < n; ++i) {
